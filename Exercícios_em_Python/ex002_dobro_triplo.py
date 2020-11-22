8ee9adbddc8a00e59b064bd05d9b8319e77f93d7
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-11-22 15:03:25 -0300

    Exercicio002
    Dizer o dobro, triplo e raiz de um número

---
+++ Exercícios_em_Python/ex002_dobro_triplo.py
@@ -0,0 +1,5 @@
+num = float(input('Digite um número:'))
+dobro = num * 2
+triplo = num * 3 
+raiz = num ** (1/2)
+print('Seu número foi {}. O dobro dele é {}, o triplo é {} e a raiz é {:.3f}.'.format(num, dobro, triplo, raiz))
