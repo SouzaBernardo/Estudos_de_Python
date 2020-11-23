28dbea0f287ab5d842907aa5d4c475979f42f500
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-11-23 07:38:43 -0300

    comentei o ex002 e criei variáveis
    separei o PRINT em mais do que um, e criei outras variáveis

--- Exercícios_em_Python/ex002_dobro_triplo.py
+++ Exercícios_em_Python/ex002_dobro_triplo.py
@@ -1,5 +1,13 @@
-num = float(input('Digite um número:'))
+# Dizer o dobro, triblo e a raiz de um número INTEIRO...
+num = int(input('Digite um número:')) # Pede o número INTEIRO
+
+# Calcular o dobro, triplo e raiz...
 dobro = num * 2
 triplo = num * 3 
 raiz = num ** (1/2)
-print('Seu número foi {}. O dobro dele é {}, o triplo é {} e a raiz é {:.3f}.'.format(num, dobro, triplo, raiz))
+
+# Dizer o resultado...
+print('Seu número escolhido foi {}.'.format(num))
+print('O DOBRO dele é {}'.format(dobro))
+print('Já, seu triplo é {}'.format(triplo))
+print('Por fim, a sua raiz é {}'.format(raiz))
