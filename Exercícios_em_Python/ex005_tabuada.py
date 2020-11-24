ceebbdb26da0c96d11f41cb9d423106888cc9de8
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-11-24 07:38:43 -0300

    ex005
    Fiz uma tabuada

---
+++ Exercícios_em_Python/ex005_tabuada.py
@@ -0,0 +1,26 @@
+# Tabuada
+print('Vamos ver a tabuada!')
+num = int(input('Qual o número que você quer saber a tabuada? (número inteiro!)'))
+print('=============')
+print('{} . 0 = {}'.format(num, num * 0)) # multiplicar por 0
+print('=============')
+print('{} . 1 = {}'.format(num, num * 1)) # multiplicar por 1
+print('=============')
+print('{} . 2 = {}'.format(num, num * 2)) # multiplicar por 2
+print('=============')
+print('{} . 3 = {}'.format(num, num * 3)) # multiplicar por 3
+print('=============')
+print('{} . 4 = {}'.format(num, num * 4)) # multiplicar por 4
+print('=============')
+print('{} . 5 = {}'.format(num, num * 5)) # multiplicar por 5
+print('=============')
+print('{} . 6 = {}'.format(num, num * 6)) # multiplicar por 6
+print('=============')
+print('{} . 7 = {}'.format(num, num * 7)) # multiplicar por 7
+print('=============')
+print('{} . 8 = {}'.format(num, num * 8)) # multiplicar por 8
+print('=============')
+print('{} . 9 = {}'.format(num, num * 9)) # multiplicar por 9 
+print('=============')
+print('{} . 10 = {}'.format(num, num * 10)) # multiplicar por 10
+print('=============')
