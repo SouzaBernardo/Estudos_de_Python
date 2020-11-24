6a260b1ef289b27beeba45a6c08df08e35219387
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-11-24 07:55:42 -0300

    ex006
    Converter Dolar para Real

---
+++ Exercícios_em_Python/ex006_converter_dolar.py
@@ -0,0 +1,10 @@
+# Converter real para dolar
+# Valor do dolar -> 5,44
+print('Vamos converter seu dinheiro?')
+din_real = float(input('Quanto queres converter para dolar? R$'))
+
+# Converter
+din_dolar = din_real / 5.44 # Converte R$ para US$
+
+# Dizer o resultado
+print('Você, com R${:.2f} é equivalente à US${:.2f}.'.format(din_real, din_dolar))
