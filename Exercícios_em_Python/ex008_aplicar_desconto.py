16cc4c71c4cf2086772cf14cba58a9885f4e6e40
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-11-25 08:30:52 -0300

    ex008
    apliquei 4 if's para caso tenha Reais etc.
    depois transformar de STR para FLOAT.
    então calcular

--- Exercícios_em_Python/ex008_aplicar_desconto.py
+++ Exercícios_em_Python/ex008_aplicar_desconto.py
@@ -1,12 +1,29 @@
-# Dizer o desconto do produto
+# DESCONTO DO PRODUTO -> calculador
+
 valor = str(input('Qual o preço do produto?')).upper().strip() # Preço
 desconto = str(input('E o desconto que será aplicado?')).upper().strip() # desconto
+
 # Caso o valor tenha reais ou R$, ignorar
+
 if valor.count('REAIS'):
-    valor = valor.replace('REAIS', '') # Substitui REAIS para ''
+    valor = valor.replace('REAIS', '') # Ignora REAIS
 if valor.count('R$'):
-    valor = valor.replace('R$', '') # Substitui R$ para ''
-# Calcula o desconto
+    valor = valor.replace('R$', '') # Ignora R$
+
+# Caso o desconto tenha % ou porcento, ignorar
+
+if desconto.count('PORCENTO'):
+    desconto = desconto.replace('PORCENTO', '') # ignora PORCENTO
+if desconto.count('%'):
+    desconto = desconto.replace('%', '')
+
+# Transformar as variáveis de STR para FLOAT
+
 valor = float(valor)
-#aplicar_desconto = valor * desconto / 100
-print(valor)
+desconto = float(desconto)
+aplicar_desconto = valor * desconto / 100 # aplicar o desconto
+
+# Dizer o resultado do desconto
+
+print(f'Seu produto de R${valor} com desconto de {desconto}')
+print(f'Ficou R${aplicar_desconto}')
