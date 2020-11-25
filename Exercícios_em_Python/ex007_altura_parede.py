452bf317d563b7044d363de783aae1b80719bcd0
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-11-25 07:50:08 -0300

    ex007
    arrumei o que estava errado antes, agora podemos saber quantos litros de tinta se precisa para pintar uma parede

--- Exercícios_em_Python/ex007_altura_parede.py
+++ Exercícios_em_Python/ex007_altura_parede.py
@@ -1,9 +1,11 @@
-# Dizer a área da parede
+# Quantos litros de tinta para pintar a área da parede
+# 1L == 2m²
 
 # Pedir altura e lagura
 alt = float(input('Altura da parede:'))
 lag = float(input('Largura da parede:'))
-# Calcular a área
-area = alt * lag / 2
-# Dizer a área
-print(f'A área é {area}')
+# Calcular a área e a necessidade de litros
+area = alt * lag / 2 # área
+tinta = area / 2 # litros para pintar
+# Dizer resultado
+print(f'A área é {area}m² e precisará {tinta}L de tinta')
