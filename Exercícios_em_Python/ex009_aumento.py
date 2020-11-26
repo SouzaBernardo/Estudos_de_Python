6388fe4e005b04436bfc8b742e1471c5a4021d32
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-11-26 07:28:07 -0300

    ex009
    acabei as condições para tirar R$ e REAIS
    percebi que é bem parecido com o EX008

--- Exercícios_em_Python/ex009_aumento.py
+++ Exercícios_em_Python/ex009_aumento.py
@@ -1,1 +1,11 @@
 # Dizer o aumento do salário
+# Pedir salário
+salario = str(input('Olá, qual seu salário atual?')).upper().strip()
+# Pedir a porcentagem de aumento
+aumento = str(input('E qual a porcentagem para o aumento?')).upper().strip()
+# Desconsiderar R$ e REAIS no salário
+if salario.count('REAIS'):
+    salario = salario.replace('REAIS', '')
+if salario.count('R$'):
+    salario = salario.replace('R$', '')
+# Desconsiderar % e PORCENTO
