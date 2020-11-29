7ddfea5600c38f42b45a4c459bff35b49bbacdd8
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-11-29 14:42:35 -0300

    ex017
    mixer

--- Exercícios_em_Python/ex017_tocar_musica.py
+++ Exercícios_em_Python/ex017_tocar_musica.py
@@ -1,7 +1,13 @@
 # Escolher e tocar a música
-'''from pygame import mixer
-mixer.init()
-mixer.music.load('The_High_Line.mp3')
-mixer.music.play()
-while(mixer.music.get_busy()): pass
-'''
+from pygame import mixer
+
+print('Escolha a música que quer tocar:')
+print('Música 1: - Drop - Anno Domini Beats')
+print('Música 2: - Forever -Anno Domini Beats ')
+print('Música 3: - Cash Machine -Anno Domini Beats')
+escolha = input('Qual a sua escolha (somente o número)?')
+if escolha != '1':
+    mixer.init()
+    mixer.music.load('ex017-drop.mp3')
+    mixer.music.play()
+    while(mixer.music.get_busy()): pass
