27f83fd3a867c4f02ec477e3bd8b6f63c6b61db9
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-11-29 13:47:32 -0300

    ex15 - sorteador
    sortiar um entre 3 alunos

--- Exercícios_em_Python/ex015_sortear_3_alunos.py
+++ Exercícios_em_Python/ex015_sortear_3_alunos.py
@@ -1,4 +1,17 @@
 # Pedir 3 nomes de alunos / grupos e sortear um deles
 from random import choice
-lista = [2, 3, 4]
-print(choice(lista))
+
+# Pedir o nome dos alunos
+aluno0 = str(input('Primeiro aluno:')) # Primeiro aluno
+aluno1 = str(input('Segundo aluno:')) # Segundo aluno
+aluno2 = str(input('Terceiro aluno:')) # Terceiro aluno
+
+# Cria lista
+lista = [aluno0, aluno1, aluno2]
+
+# Escolher um aluno da lista
+sorteado = choice(lista).upper()
+
+# Dizer o sorteado
+print(f'O aluno sorteado foi {sorteado}.')
+print('__FIM__')
