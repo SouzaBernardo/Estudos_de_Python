d12befd76352be36ad31f8c24cfd954acaf35ba8
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-11-29 14:13:25 -0300

    ex016
    sorteia a ORDEM de 4 alunos

--- Exercícios_em_Python/ex016_sortear_ordem_aluno.py
+++ Exercícios_em_Python/ex016_sortear_ordem_aluno.py
@@ -1,1 +1,17 @@
 # Pedir o nome dos alunos e dizer a ordem
+from random import shuffle
+
+# Pedir o nome dos alunos
+aluno0 = str(input('Primeiro aluno:')).capitalize() # Primeiro aluno
+aluno1 = str(input('Segundo aluno:')).capitalize() # Segundo aluno
+aluno2 = str(input('Terceiro aluno:')).capitalize() # Terceiro aluno
+aluno3 = str(input('Quarto aluno:')).capitalize() # Quarto aluno
+
+# Cria lista dos alunos
+lista = [aluno0, aluno1, aluno2, aluno3]
+
+# Embaralha a lista
+shuffle(lista)
+
+# Dizer a ordem
+print(f'A ordem do sorteio é {lista[0]}, {lista[1]}, {lista[2]}, {lista[3]}.')
