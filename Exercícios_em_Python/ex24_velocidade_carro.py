f2f0099724548ddade9e1905ec628b10f395ecff
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-01 09:28:08 -0300

    ex24
    Comentado, e coloquei a variavel {multa} na condição para >= 80
    mudei a variavel de int para float

--- Exercícios_em_Python/ex24_velocidade_carro.py
+++ Exercícios_em_Python/ex24_velocidade_carro.py
@@ -1,15 +1,25 @@
+# Analisar se há multa
+
+# Pedir nome
 print('Vamos analisar seu perfil para ver se há multa...')
 print('ANALISANDO...')
 speed = input('Qual a velocidade que seu carro ultrapassou:').strip().upper()
-nume = speed.split()
-numero = int(nume[0])
-multa = (numero - 80) * 7
-print('Você passou a {} km/h'.format(numero))
-if numero >= 80:
-    print('Você tem uma multa!')
-    print('E ela custa R${}, devido a sua velocidade que foi de {}Km/h.'.format(multa, numero))
-else:
+
+# Analisar
+nume = speed.split()  # Dividir as palavras em uma lista
+numero = float(nume[0]) # Pegar [0](que é o número) e tranforma para float
+
+print(f'Você passou a {numero} km/h.') # Indica a sua velocidade
+
+if numero >= 80.0: # Caso tenha ultrapassado o limite
+    multa = (numero - 80) * 7 # Aplica a multa
+    print('Você tem uma multa!') # Indica a multa
+    print('Ultrapassou o limite de 80Km/h.') # Indica o limite de velocidade
+    print(f'Ela custa R${multa}.') # Mostra a multa
+else: # Caso NÃO tenha ultrapassado o limite
     print('Tudo certo!')
-    print('Não ultramasse 80km/h')
+    print('Não ultramasse 80km/h.')
+    
+# FIM
 print('Lembre de usar cinto de segurança!')
-print('__<_FIM_>__')
+print('__FIM__')
