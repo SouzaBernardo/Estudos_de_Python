4a4ca880c1fc104c945851136285c7907fbc9a90
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-01 19:11:44 -0300

    ex-32
    Achei o que faltava

--- Exercícios_em_Python/ex032_cores_em_python.py
+++ Exercícios_em_Python/ex032_cores_em_python.py
@@ -1,10 +1,13 @@
 # Usar Cores no Python
+import colorama
+colorama.init()
+
 nome = 'Bernardo'
-cores = {'Limpa':'\033[4;34m',
+cores = {'Limpa':'\033[0;30m',
         'azul':'\033[34m',
         'amarelo':'\033[33m'} # Dicionário
 
-print(f'Olá, \033[4;34m{nome}\033[m')
-print('{}, {}'.format(cores['azul'], cores['amarelo']))
+print(f'Olá, \033[4;32m{nome}\033[m')
+print('{}, {},'.format(cores['azul'], cores['amarelo']))
 print(f'{cores["azul"]}, {cores["amarelo"]}')
 
