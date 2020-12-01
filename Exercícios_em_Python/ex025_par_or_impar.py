1f3c8236ea1e2f029297eeae2dff31ba7c9cf5a9
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-01 16:45:23 -0300

    ex025

--- Exercícios_em_Python/ex025_par_or_impar.py
+++ Exercícios_em_Python/ex025_par_or_impar.py
@@ -1,9 +1,17 @@
+# Sortear número e dizer se é par ou impar
 from random import randint
+
+# Sortea o número
 numero = randint(1, 100)
+
+# Informa número sorteado
 print('Sorteando um número...')
 print('O número sorteado é {}.'.format(numero))
+
+# Identificar se é ou não par
 if numero % 2 == 0:
-    print('Este número é par!')
+    print('Este número é par!') # Para PAR
 else:
-    print('Este número é impar!')
+    print('Este número é impar!') # Para IMPAR
+# FIM
 print('_FIM_')
