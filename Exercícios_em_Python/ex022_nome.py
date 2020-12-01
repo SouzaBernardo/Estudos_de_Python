b3f16f71580d4c51777b682569092d01bbc57692
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-01 08:14:38 -0300

    ex022
    Comentado e criei novas variáveis

--- Exercícios_em_Python/ex022_nome.py
+++ Exercícios_em_Python/ex022_nome.py
@@ -1,3 +1,13 @@
+# Dizer o primeiro e ultimo nome
+
+# Pdeir nome
 nome = str(input('Nome:')).strip().split()
-print(nome[0].capitalize())
-print(nome[-1].capitalize())
+
+# Identificar o primeiro e ultimo
+primeiro_nome = nome[0].capitalize()
+ultimo_nome = nome[-1].capitalize()
+
+# Resultado
+print(f'O primeiro nome é {primeiro_nome}')
+print(f'E, o ultimo nome é {ultimo_nome}')
+print('__FIM__')
