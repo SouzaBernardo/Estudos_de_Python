e76b2244ffc04a2d8be054f25037996349373315
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-01 16:51:33 -0300

    Custo de uma viagem

--- Exercícios_em_Python/ex026_200km.py
+++ Exercícios_em_Python/ex026_200km.py
@@ -1,7 +1,8 @@
-km = float(input('Digite a distância de sua viagem:'))
-if km <= 200:
-    pe = km * 0.5
-    print('Sua viagem vai custar {}'.format(pe))
+# Preço de uma viagem
+km = float(input('Digite a distância de sua viagem:')) # Pergunta a distância
+if km <= 200: # Para menor ou igual a 200
+    pe = km * 0.5 # Cada Km multiplicado por R$0,50
+    print(f'Sua viagem vai custar R${pe}.') # Indica o custo
 else:
-    pa = km * 0.45
-    print('Sua viagem vai custar {}'.format(pa))
+    pa = km * 0.45 # Cada km multiplicado por R$0.45
+    print(f'Sua viagem vai custar R${pa}.')
