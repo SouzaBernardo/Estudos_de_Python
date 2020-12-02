c562cede1a616d84817eca49b40340230f969a5c
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-02 16:11:06 -0300

    ..

--- Exercícios_em_Python/ex032_cores_em_python.py
+++ Exercícios_em_Python/ex032_cores_em_python.py
@@ -25,4 +25,4 @@ cores = {
     'amarelo':'\033[33m'}
 
 print(f'Olá!{cores["azul"]}{nome}')
-print(f'Os valores são \033[32m{a}\033[m e \033[34m{b}\033[m')
+print(f'Os valores são \033[32m{a}\033[32m e \033[34m{b}\033[m')
