72492e24af3e106c736bfe3baa1e95d2df8fc3ff
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-03 08:02:39 -0300

    ex034
    Emprestimo de casa

--- Outros_Exercicios_em_python/ex034_emprestimo_bancario.py
+++ Outros_Exercicios_em_python/ex034_emprestimo_bancario.py
@@ -1,16 +1,22 @@
 # Aprovar um empréstimo bancário
+from colorama import init
+init()
 
 # Adquirir informações
 valor_da_casa = str(input('Valor da casa que dejesa comprar: R$')).lower().strip()
 valor_do_salario = str(input('valor do seu salário: R$')).lower().strip()
 anos_a_pagar = str(input('Em quantos anos quer pagar a casa? ')).lower().strip()
 
-# Caso as respostas tenham ',' ao invés de '.'
+# Indicar erro se usar virgula ao invés de ponto
 if valor_da_casa.count(',') or valor_do_salario.count(','):
-    valor_da_casa = valor_da_casa.replace(',', '.')
-    valor_do_salario = valor_do_salario.replace(',', '.')
-    print(valor_da_casa)
-    print(valor_do_salario)
-
-# Converter valores para números
-valor_da_casa = int(valor_da_casa * 2)
+    print('\033[31m ERRO!')
+    print('Você usou virgula! ao invés disso, use ponto.')
+elif valor_da_casa.count('.') or valor_do_salario.count('.'):
+    # Converter valores para números
+    valor_da_casa = float(valor_da_casa)
+    valor_do_salario = float(valor_do_salario)
+elif anos_a_pagar.count('.'):
+    anos_a_pagar = float(anos_a_pagar)
+else: 
+    print('')
+print(valor_da_casa, valor_do_salario, anos_a_pagar)
