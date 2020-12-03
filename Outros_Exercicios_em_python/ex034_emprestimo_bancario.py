8fb2eaa73237dd9e4edc78d29f3362cf6334f3ac
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-03 18:05:50 -0300

    ex034
    logica OK, falta colocar se tiver virgula o erro, e comverter para float e int

--- Outros_Exercicios_em_python/ex034_emprestimo_bancario.py
+++ Outros_Exercicios_em_python/ex034_emprestimo_bancario.py
@@ -7,18 +7,21 @@ valor_da_casa = str(input('Valor da casa que dejesa comprar: R$')).lower().strip
 valor_do_salario = str(input('valor do seu salário: R$')).lower().strip()
 anos_a_pagar = str(input('Em quantos anos quer pagar a casa? ')).lower().strip()
 
-# 
-if valor_da_casa.count(',') or valor_do_salario.count(','):
-    print('\033[31m ERRO!')
-    print('Você usou virgula! ao invés disso, use ponto.')
-elif valor_da_casa.count('.') or valor_do_salario.count('.'):
-# Converter valores para números
-    valor_da_casa = float(valor_da_casa)
-    valor_do_salario = float(valor_do_salario)
-# Calcular aqui
-    print(valor_da_casa * valor_do_salario)
-elif anos_a_pagar.count('.'):
-    anos_a_pagar = float(anos_a_pagar)
-else: 
-    print('')
-print(valor_da_casa, valor_do_salario, anos_a_pagar)
+# Converter para numero, int ou float
+
+# Operações temporareas 
+valor_da_casa = int(valor_da_casa)
+valor_do_salario = int(valor_do_salario)
+anos_a_pagar = int(anos_a_pagar)
+
+# Calcular
+valor_por_mes_casa = anos_a_pagar * 12 # Quantos meses vai pagar
+porcentagem_do_salario = (valor_do_salario * 30) / 100 # porcentagem
+
+# Ver se pode ou não comprar a casa
+if valor_por_mes_casa >= porcentagem_do_salario:
+    print('Você não pode comprar a casa, ultrapassou os 30%.')
+else:
+    print('\033[32m TUDO CERTO.\033[m')
+    print('Você pode comprar a casa!')
+    print(f'O valor dela é \033[32m R${valor_da_casa}\033[m e você vai paragar em \033[32m {valor_por_mes_casa} meses\033[m.')
