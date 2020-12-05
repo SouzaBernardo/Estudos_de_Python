a5fa8d8cc9a030db2e71f0cff8ba062196c46253
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-05 20:22:58 -0300

    newex035

--- Outros_Exercicios_em_python/ex035_maior_menor_igual.py
+++ Outros_Exercicios_em_python/ex035_maior_menor_igual.py
@@ -4,3 +4,15 @@ print('Primeiro eu preciso que você me diga quais são eles!')
 # pedir números
 pri_num = input('Primeiro número:')
 seg_num = input('Segundo número:')
+# Considerar quem é maior e menor
+menor = pri_num
+maior = pri_num
+# Verificar que é maior
+if seg_num < pri_num: # Verificar se 
+    menor = seg_num
+elif seg_num == pri_num:
+    print('Os números são iquais')
+else:
+    maior = seg_num
+print(f'O número maior é {maior} e o menor é {menor}.')
+print('__FIM__')
