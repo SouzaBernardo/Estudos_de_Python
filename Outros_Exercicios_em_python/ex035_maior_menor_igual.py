0f7a1d8d569b4392d028c0697bf3514c1dc90ed4
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-05 16:25:11 -0300

    ex035
    criari o arquivo

---
+++ Outros_Exercicios_em_python/ex035_maior_menor_igual.py
@@ -0,0 +1,6 @@
+# Pedir dois números e dizer qual deles é maior, menor ou se são iguais 
+print('Vamos analisar dois números?!')
+print('Primeiro eu preciso que você me diga quais são eles!')
+# pedir números
+pri_num = input('Primeiro número:')
+seg_num = input('Segundo número:')
