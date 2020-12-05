3c588ceb2942ad048c222dcff15878a893a4d395
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-05 20:29:34 -0300

    ex034-comentado

--- Outros_Exercicios_em_python/ex034_num_bi_cot_hex.py
+++ Outros_Exercicios_em_python/ex034_num_bi_cot_hex.py
@@ -16,18 +16,21 @@ if escolha_da_base == 1:
     # Converter para binário
     numero_escolhido_convertido = bin(numero_escolhido)
     print('\033[32mANALISANDO...\033[m')
-    print('Convertemos seu número para \033[32mBINÁRIO\033[m!')
+    print('Convertemos seu número para \033[32mBINÁRIO\033[m!') # Indicar a operação desejada
+    # Indicar resposta
     print(f'Seu número ({numero_escolhido}) em binário é \033[32m{numero_escolhido_convertido}\033[m.')
 elif escolha_da_base == 2:
     # Converter para octal
     numero_escolhido_convertido = oct(numero_escolhido)
     print('\033[32mANALISANDO...\033[m')
-    print('Convertemos seu número para \033[32mOCTAL\033[m!')
+    print('Convertemos seu número para \033[32mOCTAL\033[m!') # Indicar a operação desejada
+    # Indicar resposta
     print(f'Seu número ({numero_escolhido}) em octal é \033[32m{numero_escolhido_convertido}\033[m.')
 else:
     # Converter para Hexadecimal
     numero_escolhido_convertido = hex(numero_escolhido)
     print('\033[32mANALISANDO...\033[m')
-    print('Convertemos seu número para \033[32mHexadecimal\033[m!')
+    print('Convertemos seu número para \033[32mHexadecimal\033[m!') # Indicar a operação desejada
+    # Indicar resposta
     print(f'Seu número ({numero_escolhido}) em Hexadecimal é \033[32m{numero_escolhido_convertido}\033[m.')
 print('\033[31m__FIM__\033[m')
