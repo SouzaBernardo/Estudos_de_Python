7678bfec34bb54bc1162c5c2889173324e0a74fa
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-09 07:12:08 -0300

    boletim

---
+++ Outros_Exercicios_em_python/ex036_nota_do_ano.py
@@ -0,0 +1,6 @@
+# Dizer a média anual de um aluno e dizer se ele passou.
+print('BOLETIM:') # inicio
+print('Para saber se você passou, preciso das suas notas do 1° e 2° semestre...')
+# Perguntar as notas
+pri_sem = input('Primeiro semestre:')
+seg_sem = input('Segundo semestre:')
