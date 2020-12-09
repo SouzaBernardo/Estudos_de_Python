31d45aed9e432388ea2126b288bbaabb4f6e5bf7
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-09 07:42:54 -0300

    ex034 comentado e colorido hehe

--- Outros_Exercicios_em_python/ex036_nota_do_ano.py
+++ Outros_Exercicios_em_python/ex036_nota_do_ano.py
@@ -1,23 +1,31 @@
 # Dizer a média anual de um aluno e dizer se ele passou.
-print('BOLETIM:') # inicio
+from colorama import init # Dar cor
+init()
+# Inicio
+print('\033[32mBOLETIM:\033[m')
 print('Para saber se você passou, preciso das suas notas do 1° e 2° semestre...')
+
 # Perguntar as notas
 pri_sem = float(input('Primeiro semestre:'))
 seg_sem = float(input('Segundo semestre:'))
+
 # Calcula a média
 med_ano = (pri_sem + seg_sem) / 2
+
 # Identificar se passou de ano
 if med_ano <= 5:
     # Se o jovem ficou de recuperação
-    print('Você REPROVOU!')
-    print(f'Tu ficou com a nota de {med_ano:.2f}.')
+    print('Você \033[31mREPROVOU\033[m!')
+    print(f'Tu ficou com a nota de \033[31m{med_ano:.2f}\033[m.')
 elif med_ano < 7:
     # Se o jovem ficou abaixo de 7
-    print('Você ficou de RECUPERAÇÃO!')
-    print(f'Devido a sua nota que foi de {med_ano}.')
+    print('Você ficou de \033[31mRECUPERAÇÃO\033[m!')
+    print(f'Devido a sua nota que foi de \033[31m{med_ano}\033[m.')
 elif med_ano < 0:
-    print('Algo está errado, tente de novo!')
+    # Coisa impossivel
+    print('\033[31mAlgo está errado, tente de novo!\033[m')
 else:
     # Caso fique assima de 7
-    print('Você ficou com a média maior que 7.')
-    print(f'TU PASSOU! Com a nota de {med_ano:.2f}.')
+    print('Você ficou com a \033[32mmédia maior que 7\033[m.')
+    print(f'TU PASSOU! Com a nota de \033[32m{med_ano:.2f}\033[m.')
+print('\033[31m__FIM__\033[m')
