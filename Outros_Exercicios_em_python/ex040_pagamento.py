2c6992ee4d2f6f3ad1aeadf438362d4ed783d124
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-14 11:08:19 -0300

    inicio

--- Outros_Exercicios_em_python/ex040_pagamento.py
+++ Outros_Exercicios_em_python/ex040_pagamento.py
@@ -1,1 +1,6 @@
 # Forma de pagamente e condições para pagamento
+print('Formas de pagamento:')
+print('[1] - ')
+print('[2] - ')
+print('[3] - ')
+forma_pagamento = input('Qual a forma de pagamento (somente o número)?')
