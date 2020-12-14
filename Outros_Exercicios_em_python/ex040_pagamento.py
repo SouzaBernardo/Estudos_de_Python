edc333dba36349de989584243ff96f75c0506c92
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-14 12:03:30 -0300

    .

--- Outros_Exercicios_em_python/ex040_pagamento.py
+++ Outros_Exercicios_em_python/ex040_pagamento.py
@@ -1,6 +1,21 @@
 # Forma de pagamente e condições para pagamento
+print('PAGAMENTO!')
+# Valor do produto
+valor_produto = input('Valor do produto: R$')
+# Adiquirir a forma de pagamento
 print('Formas de pagamento:')
-print('[1] - ')
-print('[2] - ')
-print('[3] - ')
-forma_pagamento = input('Qual a forma de pagamento (somente o número)?')
+print('[1] - à vista dinheiro/cheque: 10% de desconto')
+print('[2] - à vista dinheiro/cheque: 10% de desconto')
+print('[3] - 3x ou mais no cartão: 20% de juros')
+# Resposta
+forma_pagamento = int(input('Qual a forma de pagamento (somente o número)?'))
+# Realizar a forma de pagamento
+if forma_pagamento == 1:
+    print('Escolha 1!')
+elif forma_pagamento == 2:
+    print('Escolha 2!')
+elif forma_pagamento == 3:
+    print('Escolha 3!')
+else:
+    # Caso não tenha escolha
+    print('Você não escolheu uma opção...') 
