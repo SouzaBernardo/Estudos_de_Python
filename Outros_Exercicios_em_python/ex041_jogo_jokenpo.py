a9aca93ef902b20c768bfdca4f0f2676aa77a4ad
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-19 12:27:22 -0300

    exercicio corrigido

--- Outros_Exercicios_em_python/ex041_jogo_jokenpo.py
+++ Outros_Exercicios_em_python/ex041_jogo_jokenpo.py
@@ -1,43 +1,63 @@
 # Jogo de Pedra, papel e tesoura
-from random import choice
-
+from random import randint
+from time import sleep
 from colorama import init
 init() # Colors
 
 # Opção do computador, assim ele vai escolher qual vai jogar
 
-opcoes_computador = ['Pedra', 'Papel', 'Tesoura']
-escolha_computador = choice(opcoes_computador)
+opcoes = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2) # escolhe de 0 a 2 para dentro das opções
 
 # Pedir opção do jogador
 
 print('O computador já fez sua escolha, só falta você!')
 print('Opções:')
-print('[1] - Pedra')
-print('[2] - Papel')
-print('[3] - Tesoura')
+print('[0] - Pedra')
+print('[1] - Papel')
+print('[2] - Tesoura')
 
-escolha_jogador = int(input('Qual dessas opções você escolhe (Somente o número)? '))
+jogador = int(input('Qual a sua jogada? '))
 
-# Opção do jogador
-if escolha_jogador == 1:
-    escolha_jogador = 'Pedra'
-elif escolha_jogador == 2:
-    escolha_jogador = 'Papel'
-else:
-    escolha_jogador = 'Tesoura'
-print(escolha_computador, escolha_jogador)
-escolhas_dos_dois = [escolha_computador, escolha_jogador]
+sleep(1)
+print('Pedra')
+sleep(1)
+print('Papel')
+sleep(1)
+print('Tesoura')
+sleep(2)
 
-# Definir ganhador
-if escolha_jogador != escolha_computador:
-    if escolhas_dos_dois.count('Pedra') and escolhas_dos_dois.count('Papel'):
-        print('Papel ganhou!')
-    elif escolhas_dos_dois.count('Pedra') and escolhas_dos_dois.count('Tesoura'):
-        print('Pedra ganhou!')
-    elif escolhas_dos_dois.count('Papel') and escolhas_dos_dois.count('Tesoura'):
-        print('Tesoura ganhou!')
+print('-=' * 11)
+print(f'Computador jogou {opcoes[computador]}')
+print(f'Jogador jogou {opcoes[jogador]}') # pega na lista de opções as alternativas
+print('-=' * 11)
+
+# Resultado
+if computador == 0: # Pedra 
+    if jogador == 0: # Pedra
+        print('Empate!')
+    elif jogador == 1: # Papel
+        print('Jogador ganhou')
+    elif jogador == 2: # Tesoura
+        print('Computador Ganhou')
     else:
-        print('Erro!')
-else:
-    print('Empate')
+        print('Jogada inválida')
+elif computador == 1: # Papel
+    if jogador == 0: # Pedra 
+        print('Computador Ganhou')
+    elif jogador == 1: # Papel
+        print('Empate')
+    elif jogador == 2: # Tesoura
+        print('Jogador Ganhou')
+    else:
+        print('Jogada inválida')
+elif computador == 2: # Tesoura
+    if jogador == 0: # Pedra 
+        print('Jogador Ganhou')
+    elif jogador == 1: # Papel 
+        print('Computador Ganhou')
+    elif jogador == 2: # Tesoura
+        print('Empate')
+    else:
+        print('Jogada inválida')
+print('__FIM__')
