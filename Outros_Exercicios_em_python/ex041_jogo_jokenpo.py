32f53ce3fe434a47746fe1af298b5cec6cdf780d
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-19 11:51:37 -0300

    jogo funcionando

--- Outros_Exercicios_em_python/ex041_jogo_jokenpo.py
+++ Outros_Exercicios_em_python/ex041_jogo_jokenpo.py
@@ -1,17 +1,25 @@
 # Jogo de Pedra, papel e tesoura
 from random import choice
+
 from colorama import init
+
 init()
+
 # Opção do computador, assim ele vai escolher qual vai jogar
+
 opcoes_computador = ['Pedra', 'Papel', 'Tesoura']
 escolha_computador = choice(opcoes_computador)
+
 # Pedir opção do jogador
+
 print('O computador já fez sua escolha, só falta você!')
 print('Opções:')
 print('[1] - Pedra')
 print('[2] - Papel')
 print('[3] - Tesoura')
+
 escolha_jogador = int(input('Qual dessas opções você escolhe (Somente o número)? '))
+
 # Opção do jogador
 if escolha_jogador == 1:
     escolha_jogador = 'Pedra'
@@ -24,12 +32,13 @@ escolhas_dos_dois = [escolha_computador, escolha_jogador]
 
 # Definir ganhador
 if escolha_jogador != escolha_computador:
-    if escolhas_dos_dois == ['Pedra', 'Papel']:
+    if escolhas_dos_dois.count('Pedra') and escolhas_dos_dois.count('Papel'):
         print('Papel ganhou!')
-        print('Quem ganhou foi o PC!')
-    elif escolhas_dos_dois == ['Pedra', 'Tesoura']:
+    elif escolhas_dos_dois.count('Pedra') and escolhas_dos_dois.count('Tesoura'):
         print('Pedra ganhou!')
+    elif escolhas_dos_dois.count('Papel') and escolhas_dos_dois.count('Tesoura'):
+        print('Tesoura ganhou!')
     else:
-        print()
+        print('Erro!')
 else:
     print('Empate')
