8de7fbf9303a6228ed9ca5c4016a086c34177d90
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-21 14:25:04 -0300

    .

--- Exercícios_em_Python/ex029_meu_jeito.py
+++ Exercícios_em_Python/ex029_meu_jeito.py
@@ -1,27 +1,28 @@
 from random import randint
-
+# Criar aleatórios
 a = randint(0, 100)
 b = randint(0, 100) - a
-c = randint(0, 100) - (a and b)
-
-maior = a
-medio = a
-menor = a
-
+c = randint(0, 100) - b
+# Mostrar sorteados
 print(a, b, c)
-
 # Escolhendo o maior
 if b > a and b > c:
     maior = b
-if c > a and c > b:
+elif c > a and c > b:
     maior = c
+else:
+    maior = a
 # Escolhendo o menor
 if b < a and b < c:
     menor = b
-if c < a and c < b:
+elif c < a and c < b:
     menor = c
-
-print('A ordem crescente é {}, {} e {}.'.format(menor, medio, maior))
-print('A ordem decrescente é {}, {} e {}'.format(maior, medio, menor))
+else:
+    menor = a
+# Resultado
+print('-=-' * 10)
+print('A ordem crescente é {} e {}.'.format(menor, maior))
+print('A ordem decrescente é {} e {}'.format(maior, menor))
+print('-=-' * 10)
 print('O maior é {}'.format(maior))
 print('O menor é {}'.format(menor))
