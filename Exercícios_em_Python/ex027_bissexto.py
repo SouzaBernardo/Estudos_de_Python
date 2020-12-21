fe28b0953154f3763ade3074f6493f85090dbd05
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-21 11:28:18 -0300

    comentado e colorido

--- Exercícios_em_Python/ex027_bissexto.py
+++ Exercícios_em_Python/ex027_bissexto.py
@@ -1,6 +1,14 @@
-ano = int(input('Qual ano que você está?'))
+# Identificar ano bissexto
+from colorama import init
+init()
+print('\033[33m-=-\033[m' * 8)
+ano = int(input('Qual ano que você está? ')) # Pedir o ano
+print('\033[33m-=-' * 8)
+# Verificar se é ou não bissexto
 if ano % 4 == 0:
-    print('Você está em um ano bissexto.')
+    print('\033[mVocê \033[32mestá\033[m em um ano \033[33mbissexto.\033[m')
 else:
-    print('Você não está em um ano bissexto.')
-print('_FIM_')
+    print('\033[mVocê \033[31mnão\033[m está em um ano \033[33mbissexto.\033[m')
+# Fim
+print('\033[33m-=-' * 8)
+print('\033[31m_FIM_')
