a8bb7045ef1bcaf1be997ee7c6e2490eb54e4443
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-21 11:19:58 -0300

    reformulado

--- Exercícios_em_Python/ex026_200km.py
+++ Exercícios_em_Python/ex026_200km.py
@@ -1,22 +1,24 @@
 # Preço de uma viagem
 from time import sleep
-print('-=-' * 11)
-print('Preço da viagem')
-print('-=-' * 11)
+from colorama import init
+init()
+print('\033[33m-=-' * 11)
+print('\033[35mPreço da viagem...')
+print('\033[33m-=-\033[m' * 11)
 # Pergunta a distância
 km = str(input('Digite a distância de sua viagem: ')).strip()
 km = float(km.split()[0]) # Pega somente o número
-print('-=-' * 11)
+print('\033[33m-=-' * 11)
 sleep(0.5)
 # Time...
-print('ANALISANDO...')
-print('-=-' * 11)
+print('\033[35mANALISANDO...')
+print('\033[33m-=-\033[m' * 11)
 sleep(1)
 # Calcular preço
 if km <= 200: # Para menor ou igual a 200
-    pe = km * 0.5 # Cada Km multiplicado por R$0,50
-    print(f'Sua viagem vai custar R${pe}.') # Indica o custo
+    preco = km * 0.5 # Cada Km multiplicado por R$0,50
 else:
-    pa = km * 0.45 # Cada km multiplicado por R$0.45
-    print(f'Sua viagem vai custar R${pa}.')
-print('-=-' * 11)
+    preco = km * 0.45 # Cada km multiplicado por R$0.45
+# Indica o custo
+print(f'Sua viagem vai custar: \033[35mR${preco}')
+print('\033[33m-=-\033[m' * 11)
