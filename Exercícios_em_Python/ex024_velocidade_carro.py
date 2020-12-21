075e216df19dcf274703ebd0e3fe46b330d1d1eb
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-21 10:34:38 -0300

    exercicio corrigido, coloquei corres...

--- Exercícios_em_Python/ex024_velocidade_carro.py
+++ Exercícios_em_Python/ex024_velocidade_carro.py
@@ -1,25 +1,34 @@
 # Analisar se há multa
-
-# Pedir nome
-print('Vamos analisar seu perfil para ver se há multa...')
-print('ANALISANDO...')
-speed = input('Qual a velocidade que seu carro ultrapassou:').strip().upper()
-
-# Analisar
-nume = speed.split()  # Dividir as palavras em uma lista
-numero = float(nume[0]) # Pegar [0](que é o número) e tranforma para float
-
-print(f'Você passou a {numero} km/h.') # Indica a sua velocidade
-
-if numero >= 80.0: # Caso tenha ultrapassado o limite
-    multa = (numero - 80) * 7 # Aplica a multa
-    print('Você tem uma multa!') # Indica a multa
-    print('Ultrapassou o limite de 80Km/h.') # Indica o limite de velocidade
-    print(f'Ela custa R${multa}.') # Mostra a multa
+from time import sleep
+from colorama import init
+init()
+# Pedir velocidade
+print('\033[33m-=-\033[m' * 15)
+speed = input('Qual a velocidade que seu carro ultrapassou: ').strip()
+if speed.count(','): # Indicar que o usuário ñ pode usar vírgula
+    print('\033[31mERRO!')
+    print('\033[mNão use VÍRGULA. Use PONTO')
+else: # Caso o usuário ñ use vírgula
+    numero = float(speed.split()[0]) # Transforma o primeiro objeto em float
+# Time (Dar tempo para processar)
+print('\033[33m-=-\033[m' * 15)
+print('\033[32mANALISANDO...')
+sleep(2)
+print('\033[33m-=-\033[m' * 15)
+# Analisar se há multa
+if numero > 80.0: # Caso tenha ultrapassado o limite
+    multa = (numero - 80) * 7 # Calcular a multa
+    print('\033[31mVocê tem uma \033[36mmulta!\033[31m Ultrapassou o limite de 80Km/h.') # Indica o limite de velocidade
+    print(f'Ela custa \033[36mR${multa:.2f}!\033[m') # Mostra a multa
+elif numero == 80.0: # Caso seja 80 km ñ tem multa, mas dá um aviso
+    print('\033[31mCUIDADO!')
+    print('Você passou no \033[34mLIMITE!')
+    print('\033[32mMULTA NÃO APLICADA')
 else: # Caso NÃO tenha ultrapassado o limite
-    print('Tudo certo!')
-    print('Não ultramasse 80km/h.')
-    
+    print('\033[32mTudo certo!') # Dar o OK
+    print('Não ultramasse 80km/h.\033[m')
 # FIM
-print('Lembre de usar cinto de segurança!')
-print('__FIM__')
+print('\033[33m-=-' * 15) # Linha amarela
+print('\033[32mLembre de usar cinto de segurança!') # Linha verde, use cinto
+print('\033[33m-=-' * 15)
+print('\033[31m__FIM__\033[m') # Fim vermelho
