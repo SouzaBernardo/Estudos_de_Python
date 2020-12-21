74024fdbc1051f9fcca61418d2ac767bcd4f7782
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-21 09:47:35 -0300

    exercicio corrigido

--- Exercícios_em_Python/ex023_jogo_de_adivinha.py
+++ Exercícios_em_Python/ex023_jogo_de_adivinha.py
@@ -1,21 +1,29 @@
 # Jogo de Adivinha
-from random import randint 
+from random import randint
 from colorama import init
+from time import sleep
 init()
-
 # Número entre 0 e 5
 num = randint(0, 5)
-
 # Dar o desafio
+print('\033[33m-=-\033[m' * 9)
 print('Escolhi um número de 0 à 5.')
+print('\033[33m-=-\033[m' * 9)
+# Resposta do usuário
 pergunta = int(input(('Sabe qual é?')))
-
+# Tempo...
+print('\033[33mProcessando...')
+sleep(2)
+print('Quase lá...\033[m')
+sleep(2)
 # Condições
-if pergunta == num:
-    print('\033[32mAcertou!!!') # Se acertar o número
-else:
+print('\033[32m-=-\033[m' * 9)
+if pergunta == num: # caso ganhe
+    print('\033[32mAcertou!!!\033[m') # Se acertar o número
+else: # Caso erre
     print('\033[31mErrou!!!\033[m Tente novamente...') # Se errar o número
-
 # Resultado
-print(f'O número era {num}')
-print('__FIM__')
+print('\033[32m-=-\033[m' * 9)
+print(f'O número era \033[32m{num}\033[m') # Informa qual era o número
+print('\033[32m-=-\033[m' * 9)
+print('\033[31m__FIM__\033[m')
