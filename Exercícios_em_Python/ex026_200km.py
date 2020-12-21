3d168cea585b6fdadba69dec88e4f65baae82ace
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-21 11:08:40 -0300

    exercicio feit, falta colocar as cores

--- Exercícios_em_Python/ex026_200km.py
+++ Exercícios_em_Python/ex026_200km.py
@@ -1,8 +1,22 @@
 # Preço de uma viagem
-km = float(input('Digite a distância de sua viagem:')) # Pergunta a distância
+from time import sleep
+print('-=-' * 11)
+print('Preço da viagem')
+print('-=-' * 11)
+# Pergunta a distância
+km = str(input('Digite a distância de sua viagem: ')).strip()
+km = float(km.split()[0]) # Pega somente o número
+print('-=-' * 11)
+sleep(0.5)
+# Time...
+print('ANALISANDO...')
+print('-=-' * 11)
+sleep(1)
+# Calcular preço
 if km <= 200: # Para menor ou igual a 200
     pe = km * 0.5 # Cada Km multiplicado por R$0,50
     print(f'Sua viagem vai custar R${pe}.') # Indica o custo
 else:
     pa = km * 0.45 # Cada km multiplicado por R$0.45
     print(f'Sua viagem vai custar R${pa}.')
+print('-=-' * 11)
