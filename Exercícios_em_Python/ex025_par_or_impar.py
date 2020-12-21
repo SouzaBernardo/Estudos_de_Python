f1853b669dc6e473746c07ecaca083765d66af43
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2020-12-21 10:53:18 -0300

    exercicio bonitinho com corres

--- Exercícios_em_Python/ex025_par_or_impar.py
+++ Exercícios_em_Python/ex025_par_or_impar.py
@@ -1,17 +1,28 @@
 # Sortear número e dizer se é par ou impar
 from random import randint
-
+from time import sleep
+from colorama import init
+init()
 # Sortea o número
 numero = randint(1, 100)
-
+print('\033[33m-=-' * 8)
+print('\033[32mSorteando um número...')
+print('\033[33m-=-' * 8)
+sleep(1)
 # Informa número sorteado
-print('Sorteando um número...')
-print('O número sorteado é {}.'.format(numero))
-
+print(f'\033[mO número sorteado é \033[32m{numero}\033[m.')
+print('\033[33m-=-' * 8)
+sleep(1)
+# Indicar que o PC está pensando
+print('\033[32mANALISANDO...')
+print('\033[33m-=-\033[m' * 8)
+sleep(1)
 # Identificar se é ou não par
 if numero % 2 == 0:
-    print('Este número é par!') # Para PAR
+    print('Este número é \033[32mpar!') # Para PAR
 else:
-    print('Este número é impar!') # Para IMPAR
+    print('Este número é \033[32mimpar!') # Para IMPAR
 # FIM
-print('_FIM_')
+print('\033[33m-=-' * 8)
+print('\033[31m_FIM_')
+print('\033[33m-=-' * 8)
