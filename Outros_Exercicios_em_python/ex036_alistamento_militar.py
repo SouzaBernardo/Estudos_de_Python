3ae984f1b5fbd6557d637cd4ca7657f659997059
Bernardo P S Souza <beportoss@gmail.com>
2021-01-02 21:43:45 -0300

    corrigido com o video

--- Outros_Exercicios_em_python/ex036_alistamento_militar.py
+++ Outros_Exercicios_em_python/ex036_alistamento_militar.py
@@ -1,13 +1,15 @@
 # Verificar quando deve se alistar e dar o tempo
+from datetime import date # importar tempo do sistema
+ano_atual = date.today().year # data do pc, hoje().ano
 print('Vamos ver quando você deve se alistar no exercito?')
-# Verificar ano de nascimento
+# ano de nascimento
 ano_nascimento = int(input('Qual ano você nasceu?'))
-# Verificar qual a idade
-idade_do_individuo = 2020 - ano_nascimento # Identificar a idade do jovem
+# qual a idade
+idade_do_individuo = int(ano_atual) - ano_nascimento # Identificar a idade do jovem
 tempo_de_alistamento = idade_do_individuo - 18 # Saber quantos anos a pessoa se alistou
-# Identificar o caso do individuo
+# indicar a situação do individuo
 print('-=' * 10)
-print(f'Quem nasceu em {ano_nascimento} tem {idade_do_individuo} anos em 2020')
+print(f'Quem nasceu em {ano_nascimento} tem ou terá {idade_do_individuo} anos em {ano_atual}')
 print('-=' * 10)
 # Maior de idade anos
 if idade_do_individuo > 18: 
@@ -15,11 +17,11 @@ if idade_do_individuo > 18:
         print('Está na hora de se alistar!')
     else: # Se for maior de 18
         print('Você já passou da hora de se alistar!')
-        print(f'Seu alistamento foi à {tempo_de_alistamento} anos atrás.')
+        print(f'Seu alistamento foi há {tempo_de_alistamento} anos atrás.')
 else:
     # Se for menor de 18
     tempo_de_alistamento = 18 - idade_do_individuo # inverter para o resultado ser +
-    ano_alistamento = 2020 + tempo_de_alistamento
+    ano_alistamento = ano_atual + tempo_de_alistamento
     print('Este ano você não deve se alistar!')
     print(f'Você deve se alistar daqui a {tempo_de_alistamento} anos, em {ano_alistamento}.')
 print('__FIM__')
