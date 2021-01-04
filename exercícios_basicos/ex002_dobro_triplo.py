0ba05aae6e35b1b2717b458fef40738bf19a71f5
Bernardo P S Souza <beportoss@gmail.com>
2021-01-04 09:33:49 -0300

    ex002 colorido e a raiz foi formatada para 2 casas

--- exercícios_basicos/ex002_dobro_triplo.py
+++ exercícios_basicos/ex002_dobro_triplo.py
@@ -1,13 +1,17 @@
 # Dizer o dobro, triblo e a raiz de um número INTEIRO...
-num = int(input('Digite um número:')) # Pede o número INTEIRO
-
-# Calcular o dobro, triplo e raiz...
+cores = {
+    'sem_cor':'\033[m',
+    'amarelo':'\033[33m',
+    'vermelho':'\033[31m',
+    'azul':'\033[34m',
+    'verde':'\033[32m'
+}
+num = int(input('Digite um número:'))
 dobro = num * 2
 triplo = num * 3 
 raiz = num ** (1/2)
-
-# Dizer o resultado...
-print('Seu número escolhido foi {}.'.format(num))
-print('O DOBRO dele é {}'.format(dobro))
-print('Já, seu triplo é {}'.format(triplo))
-print('Por fim, a sua raiz é {}'.format(raiz))
+# resultados
+print(f'Seu número escolhido foi {cores["amarelo"]}{num}{cores["sem_cor"]}.')
+print(f'O DOBRO dele é {cores["vermelho"]}{dobro}{cores["sem_cor"]}')
+print(f'Já, seu triplo é {cores["azul"]}{triplo}{cores["sem_cor"]}')
+print(f'Por fim, a sua raiz é {cores["verde"]}{raiz:.2f}{cores["sem_cor"]}')
