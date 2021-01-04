636c96c649dec039cd78e61277caa279b2de469e
Bernardo P S Souza <beportoss@gmail.com>
2021-01-04 09:24:13 -0300

    ex001 mudado de novo, criei uma lista com as cores

--- exercícios_basicos/ex001_analisar_numer.py
+++ exercícios_basicos/ex001_analisar_numer.py
@@ -1,15 +1,16 @@
 # Pedir um número e dizer seu sucessor e antecessor
-numero = int(input('\033[33mDigite um número inteiro:\033[m'))
+cores = {
+    'sem_cor':'\033[m',
+    'amarelo':'\033[33m',
+    'vermelho':'\033[31m',
+    'azul':'\033[34m'
+}
+numero = int(input(f'{cores["amarelo"]}Digite um número inteiro:'))
 sucessor = numero + 1 
 antecessor = numero - 1 
-# cores
-sem_cor = '\033[m'
-amarelo = '\033[33m'
-vermelho = '\033[31m'
-azul = '\033[34m'
+print('=/' * 9 + f'={cores["sem_cor"]}')
+print(f'Seu número foi {cores["amarelo"]}{numero}{cores["sem_cor"]};')
+print(f'O {cores["vermelho"]}antecessor{cores["sem_cor"]} desse número é {cores["vermelho"]}{antecessor}{cores["sem_cor"]};')
+print(f'O {cores["azul"]}sucessor{cores["sem_cor"]} é {cores["azul"]}{sucessor}{cores["amarelo"]}.') 
 print('=/' * 9 + '=')
-print(f'Seu número foi {amarelo}{numero}{sem_cor};')
-print(f'O {vermelho}antecessor{sem_cor} desse número é {vermelho}{antecessor}{sem_cor};')
-print(f'O {azul}sucessor{sem_cor} é {azul}{sucessor}{sem_cor}.') 
-print('=/' * 9 + '=')
-print('\033[33m__FIM__\033[m')
+print(f'__FIM__{cores["sem_cor"]}')
