f3186d9c824b9f91534ec8c84cdb838a90f876a1
Bernardo P S Souza <beportoss@gmail.com>
2021-01-05 20:14:57 -0300

    colorido e formatado

--- exercícios_basicos/ex004_converter_metros.py
+++ exercícios_basicos/ex004_converter_metros.py
@@ -1,18 +1,13 @@
 # Converter Metros para cm e mm
-print('Vamos converter!')
-
 # Unidades de medida 
     # 1m == 100 cm
     # 1m == 1000mm
+print('Vamos converter!')
 
-# Pedir valor
-metro_converter = float(input('Quantos metros queres converter?'))
-
-# Calcular a conversão
+metro_converter = float(input('\033[33mQuantos metros queres converter?\033[m'))
 cm_convertido = metro_converter * 100 # converter para cm  
 mm_convertido = metro_converter * 1000 # converter para mm
 
-# Dizer o resultado
-print('Sua metragem foi {}m'.format(metro_converter))
-print('Em cm, converte-se para {}cm'.format(cm_convertido))
-print('Já, em mm é convertido para {}mm'.format(mm_convertido))
+print(f'Sua metragem foi \033[33m{metro_converter}m\033[m')
+print(f'Em cm, converte-se para \033[33m{cm_convertido}cm\033[m')
+print(f'Já, em mm é convertido para \033[33m{mm_convertido}mm\033[m')
