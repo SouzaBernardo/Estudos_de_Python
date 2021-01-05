e0b10b5fe20212f4b48295c2b51030d7c17601f0
Bernardo P S Souza <beportoss@gmail.com>
2021-01-05 20:20:55 -0300

    colorido

--- exercícios_basicos/ex005_tabuada.py
+++ exercícios_basicos/ex005_tabuada.py
@@ -1,26 +1,27 @@
 # Tabuada
 print('Vamos ver a tabuada!')
+print('=' * 13)
 num = int(input('Qual o número que você quer saber a tabuada? (número inteiro!)'))
-print('=============')
-print('{} . 0 = {}'.format(num, num * 0)) # multiplicar por 0
-print('=============')
-print('{} . 1 = {}'.format(num, num * 1)) # multiplicar por 1
-print('=============')
-print('{} . 2 = {}'.format(num, num * 2)) # multiplicar por 2
-print('=============')
-print('{} . 3 = {}'.format(num, num * 3)) # multiplicar por 3
-print('=============')
-print('{} . 4 = {}'.format(num, num * 4)) # multiplicar por 4
-print('=============')
-print('{} . 5 = {}'.format(num, num * 5)) # multiplicar por 5
-print('=============')
-print('{} . 6 = {}'.format(num, num * 6)) # multiplicar por 6
-print('=============')
-print('{} . 7 = {}'.format(num, num * 7)) # multiplicar por 7
-print('=============')
-print('{} . 8 = {}'.format(num, num * 8)) # multiplicar por 8
-print('=============')
-print('{} . 9 = {}'.format(num, num * 9)) # multiplicar por 9 
-print('=============')
-print('{} . 10 = {}'.format(num, num * 10)) # multiplicar por 10
-print('=============')
+print('=' * 13)
+print('\033[33m{num} . 0 = {num * 0}\033[m')
+print('=' * 13)
+print('\033[33m{num} . 1 = {num * 1}\033[m')
+print('=' * 13)
+print('\033[33m{num} . 2 = {num * 2}\033[m')
+print('=' * 13)
+print('\033[33m{num} . 3 = {num * 3}\033[m')
+print('=' * 13)
+print('\033[33m{num} . 4 = {num * 4}\033[m')
+print('=' * 13)
+print('\033[33m{num} . 5 = {num * 5}\033[m')
+print('=' * 13)
+print('\033[33m{num} . 6 = {num * 6}\033[m')
+print('=' * 13)
+print('\033[33m{num} . 7 = {num * 7}\033[m')
+print('=' * 13)
+print('\033[33m{num} . 8 = {num * 8}\033[m')
+print('=' * 13)
+print('\033[33m{num} . 9 = {num * 9}\033[m')
+print('=' * 13)
+print('\033[33m{num} . 10 = {num * 10}\033[m')
+print('=' * 13)
