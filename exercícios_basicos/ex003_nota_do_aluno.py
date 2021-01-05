75187ec20bbb48a7eee9c1c4609f370f7a7ec771
Bernardo P S Souza <beportoss@gmail.com>
2021-01-05 20:11:11 -0300

    colorido e diz se passou ou nao

--- exercícios_basicos/ex003_nota_do_aluno.py
+++ exercícios_basicos/ex003_nota_do_aluno.py
@@ -1,16 +1,21 @@
 # Realizar a média anual de um aluno...
+cores = {
+    'sem_cor':'\033[m',
+    'amarelo':'\033[33m',
+    'vermelho':'\033[31m',
+    'azul':'\033[34m',
+    'verde':'\033[32m'
+}
+print(f'{cores["amarelo"]}Olá, vamos descobrir a sua média no ano?{cores["sem_cor"]}') # Dar olá para pessoa
+primeira_nota = float(input(f'Qual a sua nota {cores["amarelo"]}TOTAL{cores["sem_cor"]} do {cores["amarelo"]}primeiro semestre{cores["sem_cor"]}?'))
+segunda_nota = float(input(f'E a do {cores["azul"]}segundo semestre{cores["sem_cor"]}? Qual a sua nota?'))
+media_resposta = (primeira_nota + segunda_nota) / 2 # calculo da media
 
-print('Olá, vamos descobrir a sua média no ano?') # Dar olá para pessoa
-
-# Atribuir valor dos semestres para as variáveis
-nota_primeiro = float(input('Qual a sua nota TOTAL do primeiro semestre?')) # Pedir o primeiro valor, a nota do 1° semestre
-nota_segundo = float(input('E a do segundo semestre? Qual a sua nota?')) # pedir o segundo valor, a nota do 2° semestre
-media_resposta = (nota_primeiro + nota_segundo) / 2
-
-# Dizer ao user qual é as notas e a sua média...
-print('Sua nota do primeiro semestre foi {:.1f}'.format(nota_primeiro))
-print('E, sua nota do segundo semestre foi {:.1f}'.format(nota_segundo))
-print('Portanto, sua media deste ano foi {:.1f}.'.format(media_resposta))
-
-# __FIM__
+print(F'Sua nota do {cores["amarelo"]}primeiro semestre{cores["sem_cor"]} foi {cores["amarelo"]}{primeira_nota:.1f}{cores["sem_cor"]}')
+print(f'E, sua nota do {cores["azul"]}segundo semestre{cores["sem_cor"]} foi {cores["azul"]}{segunda_nota:.1f}{cores["sem_cor"]}')
+print(f'Portanto, sua {cores["verde"]}media{cores["sem_cor"]} deste ano foi {cores["verde"]}{media_resposta:.1f}{cores["sem_cor"]}.')
+if media_resposta >= 7:
+    print(f'{cores["verde"]}APROVADO!!!{cores["sem_cor"]}')
+else:
+    print(f'Você está de {cores["vermelho"]}RECUPERAÇÃO{cores["sem_cor"]}.')
 print('__FIM__')
