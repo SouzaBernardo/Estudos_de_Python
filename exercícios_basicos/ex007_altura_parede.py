33ab1eac34a23b88b66f185011db82c54e853514
Bernardo P S Souza <beportoss@gmail.com>
2021-01-07 14:02:00 -0300

    colorido

--- exercícios_basicos/ex007_altura_parede.py
+++ exercícios_basicos/ex007_altura_parede.py
@@ -1,11 +1,9 @@
 # Quantos litros de tinta para pintar a área da parede
 # 1L == 2m²
-
-# Pedir altura e lagura
+print('\033[33mVAMOS PINTAR A PAREDE!!!\033[m')
 alt = float(input('Altura da parede:'))
 lag = float(input('Largura da parede:'))
-# Calcular a área e a necessidade de litros
-area = alt * lag / 2 # área
-tinta = area / 2 # litros para pintar
-# Dizer resultado
-print(f'A área é {area}m² e precisará {tinta}L de tinta')
+area = alt * lag / 2 
+tinta = area / 2 # calcula a quantidade de tinta
+print(f'A área é \033[33m{area}m²\033[m e precisará \033[33m{tinta}L de tinta\033[m para pinta-la.')
+print('__FIM__')
