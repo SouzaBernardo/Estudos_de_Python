e4fcf947a51a7679551a1dddbdd8d41ec7eee88e
Bernardo P S Souza <beportoss@gmail.com>
2021-01-08 12:39:09 -0300

    colorido

--- exercícios_basicos/ex011_aluguel_carro.py
+++ exercícios_basicos/ex011_aluguel_carro.py
@@ -1,17 +1,23 @@
 # Aluguel de carro
+
 from math import ceil
+from time import sleep
 
-dias_alugados = str(input('Quantos dias foram alugados?')).lower().strip() # Pedir dias
+dias_alugados = str(input('\033[33mQuantos dias foram alugados?\033[m')).strip().split() # Pedir dias
+print('-=' * 25)
+print('\033[31m(Caso tenha usado metade de um dia, o valor será arredondado!)\033[m') # Informação, arredondar
+print('-=' * 25)
+sleep(1.5)
+km_rodados = str(input('\033[33mQuantos quilômetros foram rodados?\033[m')).strip().split() # Pedir Km
 
-print('(Caso tenha usado metade de um dia, o valor será arredondado!)') # Informação, arredondar
+dias_alugados = float(dias_alugados[0])
+km_rodados = float(km_rodados[0])
 
-km_rodados = str(input('Quantos quilômetros foram rodados?')).lower().strip() # Pedir Km
-
-dias_alugados = float(dias_alugados.split()[0])
-km_rodados = float(km_rodados.split()[0])
-
-dias_alugados = ceil(dias_alugados) # aqui arredonda os dias
+dias_alugados = ceil(dias_alugados) # arredonda os dias
 preco_aluguel = (dias_alugados * 60) + (km_rodados * 0.15)
 
-print(f'Rodando {dias_alugados} dias (PODE ESTAR ARREDONDADO) e {km_rodados} quilômetros.')
-print(f'Seu aluguel ficou com o preço de R${preco_aluguel}.')
+print('-=' * 25)
+print(f'Rodando \033[32m{dias_alugados}\033[m dias \033[31m(PODE ESTAR ARREDONDADO)\033[m e \033[32m{km_rodados}\033[m quilômetros.')
+print(f'Seu aluguel ficou com o preço de \033[33mR${preco_aluguel}\033[m.')
+print('-=' * 25)
+print('__FIM__')
