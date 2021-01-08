e21952a6da9a119db8e230d56c0118f8b7556eca
Bernardo P S Souza <beportoss@gmail.com>
2021-01-08 12:29:06 -0300

    colorido

--- exercícios_basicos/ex010_fahrenheit.py
+++ exercícios_basicos/ex010_fahrenheit.py
@@ -1,20 +1,17 @@
 # Transformar grau Celsius para Grau Fahrenheit
-
-# Pedir temp. em °C
-grau_cel = str(input('Informe a temperatura em Grau Celsius:')).strip()
-
-# Pegar primeiro número, ignorando coisas como GRAUS
-lista = grau_cel.split() # Cria um array com o número e seus complementos
-so_numero = lista[0] # Pega somente o número
-so_numero = float(so_numero) # Transforma o número em Float
-
-# Aplicar calculos para converter
-grau_fah = 9 * so_numero / 5 + 32 # Fahrenheit
-kelvin = so_numero + 273.15 # Kelvin
-
-# Resultado...
-print(f'A temperatura em Grau Celsius é {so_numero} °C')
-print('CONVERTENDO...')
-print(f'Temperatura convertida para Grau Fahrenheit: {grau_fah}')
-print(f'Temperatura convertida para Kelvin: {kelvin}K')
+from time import sleep
+grau_cel = str(input('\033[33mInforme a temperatura em Grau Celsius:\033[m')).strip()
+lista = grau_cel.split()  # Cria um array com o número e seus complementos
+so_numero = lista[0]  # Pega o primeiro valor, o número
+so_numero = float(so_numero)
+grau_fah = 9 * so_numero / 5 + 32  # calcula Fahrenheit
+kelvin = so_numero + 273.15  # calcula Kelvin
+print(f'A temperatura em \033[33mGrau Celsius\033[m é \033[33m{so_numero} °C\033[m.')
+sleep(1)
+print('\033[31mCONVERTENDO...\033[m')
+sleep(1)
+print('\033[31m-=' * 10 + '\033[m')
+print(f'Temperatura convertida para \033[32mGrau Fahrenheit: {grau_fah}°F\033[m;')
+print(f'Temperatura convertida para \033[32mKelvin: {kelvin}°K\033[m;')
+print('\033[31m-=' * 10 + '\033[m')
 print('__FIM__')
