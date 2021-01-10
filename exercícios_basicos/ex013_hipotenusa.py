4d1b3cab9aada0d50956bad6441bf77382c3284b
Bernardo P S Souza <beportoss@gmail.com>
2021-01-09 22:02:51 -0300

    colorido

--- exercícios_basicos/ex013_hipotenusa.py
+++ exercícios_basicos/ex013_hipotenusa.py
@@ -1,11 +1,13 @@
 # Calculadora de hipotenusa
+
 from math import hypot
 
-# Pedir os catetos
-cateto_ad = float(input('Cateto adjacente:')) # Cateto Adjacente
-cateto_op = float(input('Cateto oposto:')) # Cateto oposto
+cateto_ad = float(input('\033[33mCateto adjacente:\033[m'))
+cateto_op = float(input('\033[33mCateto oposto:\033[m'))
 hipo = hypot(cateto_ad, cateto_op) # Calcular a hipotenusa
 
-# Resultado
-print(f'A hipotenusa, do cateto oposto de valor {cateto_op} e cateto adjacente de valor {cateto_ad}, é: {hipo :.2f}')
-print('__FIM__')
+print('-=' * 45)
+print(f'\033[mA \033[33mhipotenusa\033[m, do cateto oposto de valor {cateto_op} e cateto adjacente de valor {cateto_ad}, é: \033[33m{hipo :.2f}\033[m.')
+print('-=' * 45)
+
+print('\033[31m__FIM__\033[m')
