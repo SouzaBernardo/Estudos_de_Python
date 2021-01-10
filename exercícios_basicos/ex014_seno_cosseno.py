371035ffa7141ce6bc0c0bdc5b954454abb53009
Bernardo P S Souza <beportoss@gmail.com>
2021-01-09 22:07:19 -0300

    colorido

--- exercícios_basicos/ex014_seno_cosseno.py
+++ exercícios_basicos/ex014_seno_cosseno.py
@@ -1,18 +1,15 @@
 # Pedir o angulo e dizer qual é o seno, conseno e tangente
 from math import radians, sin, cos, tan
 
-# Pedi o angulo
-angulo = float(input('Angulo que você deseja:'))
+angulo = float(input('\033[33mAngulo que você deseja:\033[m'))
 
-# Cacular os resultados
 seno = sin(radians(angulo))
 cosseno = cos(radians(angulo))
 tangente = tan(radians(angulo))
 
-# Dar os resultados
-print(f'O seno é {seno}.')
-print(f'O cosseno é {cosseno}.')
-print(f'A tangente é {tangente}.')
-
-# Fim do programa
+print('-=' * 8)
+print(f'O \033[32mseno\033[m: \033[34m{seno:.2f}\033[m.')
+print(f'O \033[32mcosseno\033[m: \033[34m{cosseno:.2f}\033[m.')
+print(f'A \033[32mtangente\033[m: \033[34m{tangente:.2f}\033[m.')
+print('-=' * 8)
 print('__FIM__')
