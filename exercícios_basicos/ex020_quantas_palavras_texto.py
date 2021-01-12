5206b50f09a2e9e28d29add7996f6b55f62e0777
Bernardo P S Souza <beportoss@gmail.com>
2021-01-12 18:26:36 -0300

    colorido, reformulado e com a ideia de dizerolocal

--- exercícios_basicos/ex020_quantas_palavras_texto.py
+++ exercícios_basicos/ex020_quantas_palavras_texto.py
@@ -1,13 +1,7 @@
 # Quantas palavras tem em um texto
-
-# Pedir palavra
-palavra = str(input('Qual palavra você quer saber a quantidade?'))
-
-# Pedir texto
-print('DIGITE O TEXTO ABAIXO:')
+palavra = str(input('\033[33mQual palavra você quer saber a quantidade?\033[m'))
+print('\033[31mDIGITE O TEXTO ABAIXO:\033[m')
 texto = str(input(''))
-
-quantidade_palavra = texto.count(palavra) # Analisar quantas palavras tem
-
-# Resultado
-print(f'A palavra {palavra} no seu texto repete {quantidade_palavra} vezes.')
+quantidade_palavra = texto.count(palavra) 
+# Ideia: dizer a posição onde as palavras se encontram
+print(f'A palavra \033[33m{palavra}\033[m no seu texto repete \033[33m{quantidade_palavra}\033[m vezes.')
