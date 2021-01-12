192dadcec19349f64fe7e4aad00ce54b1324fc30
Bernardo P S Souza <beportoss@gmail.com>
2021-01-12 18:21:25 -0300

    colorido

--- exercícios_basicos/ex019_analisar_cidade.py
+++ exercícios_basicos/ex019_analisar_cidade.py
@@ -1,8 +1,8 @@
 # Analisar o nome da cidade
-nome = str(input('Digite o nome da sua cidade:')).upper().strip()
+nome = str(input('\033[33mDigite o nome da sua cidade:\033[m')).upper().strip()
 if nome.count('SANTO'):
     vezes_que_aparece = nome.count('SANTO')
-    print('Sua cidade tem em seu nome a palavra "SANTO"!') 
-    print(f'Essa palavra aparece {vezes_que_aparece} vezes.')
+    print('\033[32mSua cidade tem em seu nome a palavra "SANTO"!\033[m') 
+    print(f'Essa palavra \033[33maparece {vezes_que_aparece} vezes\033[m.')
 else:
-    print('Sua cidade não contém a palavra "SANTO"!')
+    print('\033[31mSua cidade não contém a palavra "SANTO"!\033[m')
