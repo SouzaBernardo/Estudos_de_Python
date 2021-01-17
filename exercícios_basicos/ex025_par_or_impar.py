4607d1bed731e3d5cc14f8c72fa6c27c76c2cb5c
Bernardo P S Souza <beportoss@gmail.com>
2021-01-17 15:37:51 -0300

    colorido e diminui os comentários

--- exercícios_basicos/ex025_par_or_impar.py
+++ exercícios_basicos/ex025_par_or_impar.py
@@ -1,28 +1,30 @@
 # Sortear número e dizer se é par ou impar
 from random import randint
 from time import sleep
-from colorama import init
-init()
-# Sortea o número
+
 numero = randint(1, 100)
-print('\033[33m-=-' * 8)
-print('\033[32mSorteando um número...')
-print('\033[33m-=-' * 8)
+
+cores = {
+    'limpo':'\033[m',
+    'verde':'\033[32m',
+    'amarelo':'\033[33m',
+}
+linha_amarela = f'{cores["amarelo"]}-=-' * 8 + f'{cores["limpo"]}'
+
+print(linha_amarela)
+print(f'{cores["verde"]}Sorteando um número...')
+print(linha_amarela)
 sleep(1)
-# Informa número sorteado
-print(f'\033[mO número sorteado é \033[32m{numero}\033[m.')
-print('\033[33m-=-' * 8)
+print(f'{cores["limpo"]}Ele é {cores["verde"]}{numero}{cores["limpo"]}.')
+print(linha_amarela)
 sleep(1)
-# Indicar que o PC está pensando
-print('\033[32mANALISANDO...')
-print('\033[33m-=-\033[m' * 8)
+print(f'{cores["verde"]}ANALISANDO...{cores["limpo"]}')
+print(linha_amarela)
 sleep(1)
-# Identificar se é ou não par
+
 if numero % 2 == 0:
-    print('Este número é \033[32mpar!') # Para PAR
+    print(f'Este número é {cores["verde"]}par!{cores["limpo"]}')
 else:
-    print('Este número é \033[32mimpar!') # Para IMPAR
-# FIM
-print('\033[33m-=-' * 8)
-print('\033[31m_FIM_')
-print('\033[33m-=-' * 8)
+    print(f'Este número é {cores["verde"]}impar!{cores["limpo"]}')
+
+print(linha_amarela)
