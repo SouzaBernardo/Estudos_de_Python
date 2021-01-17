e19aa2200563f266d83cd79a881b083e6bedafa3
Bernardo P S Souza <beportoss@gmail.com>
2021-01-17 15:55:41 -0300

    coloorido e diminui os comentarios

--- exercícios_basicos/ex027_bissexto.py
+++ exercícios_basicos/ex027_bissexto.py
@@ -1,14 +1,20 @@
 # Identificar ano bissexto
-from colorama import init
-init()
-print('\033[33m-=-\033[m' * 8)
-ano = int(input('Qual ano que você está? ')) # Pedir o ano
-print('\033[33m-=-' * 8)
-# Verificar se é ou não bissexto
+cores = {
+    'limpo':'\033[m',
+    'amarelo':'\033[33m',
+    'verde':'\033[32m',
+    'vermelho':'\033[31m',
+}
+
+linha_amarela = f'{cores["amarelo"]}-=-' * 8 + f'{cores["limpo"]}'
+
+print(linha_amarela)
+ano = int(input('Qual ano que você está? '))
+print(linha_amarela)
+
 if ano % 4 == 0:
-    print('\033[mVocê \033[32mestá\033[m em um ano \033[33mbissexto.\033[m')
+    print(f'Você {cores["verde"]}está{cores["limpo"]} em um ano {cores["amarelo"]}bissexto{cores["limpo"]}.')
 else:
-    print('\033[mVocê \033[31mnão\033[m está em um ano \033[33mbissexto.\033[m')
-# Fim
-print('\033[33m-=-' * 8)
-print('\033[31m_FIM_')
+    print(f'Você {cores["vermelho"]}não{cores["limpo"]} está em um ano {cores["amarelo"]}bissexto{cores["limpo"]}.')
+
+print(linha_amarela)
