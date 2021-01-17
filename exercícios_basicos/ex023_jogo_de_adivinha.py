a3223f4a1277f47dd97a643a1af1498b56e57332
Bernardo P S Souza <beportoss@gmail.com>
2021-01-16 22:16:20 -0300

    colorido e criei melhores linhas

--- exercícios_basicos/ex023_jogo_de_adivinha.py
+++ exercícios_basicos/ex023_jogo_de_adivinha.py
@@ -1,29 +1,38 @@
 # Jogo de Adivinha
 from random import randint
-from colorama import init
 from time import sleep
-init()
-# Número entre 0 e 5
 num = randint(0, 5)
-# Dar o desafio
-print('\033[33m-=-\033[m' * 9)
-print('Escolhi um número de 0 à 5.')
-print('\033[33m-=-\033[m' * 9)
-# Resposta do usuário
-pergunta = int(input(('Sabe qual é?')))
-# Tempo...
-print('\033[33mProcessando...')
-sleep(2)
-print('Quase lá...\033[m')
-sleep(2)
-# Condições
-print('\033[32m-=-\033[m' * 9)
-if pergunta == num: # caso ganhe
-    print('\033[32mAcertou!!!\033[m') # Se acertar o número
-else: # Caso erre
-    print('\033[31mErrou!!!\033[m Tente novamente...') # Se errar o número
-# Resultado
-print('\033[32m-=-\033[m' * 9)
-print(f'O número era \033[32m{num}\033[m') # Informa qual era o número
-print('\033[32m-=-\033[m' * 9)
+cores = {
+    'limpo':'\033[m',
+    'amarelo':'\033[33m',
+    'verde':'\033[32m',
+    'vermelho':'\033[31m',
+}
+
+linha_verde = f'{cores["verde"]}-=-' * 9 + f'{cores["limpo"]}'
+linha_amarela = f'{cores["amarelo"]}-=-' * 9 + f'{cores["limpo"]}'
+
+print(linha_amarela)
+print('Escolhi um número de 0 até 5.')
+print(linha_amarela)
+
+pergunta = int(input(f'{cores["verde"]}Sabe qual é? '))
+
+print(linha_amarela)
+sleep(1)
+print(f'{cores["amarelo"]}Processando...')
+sleep(1)
+print(f'Quase lá...{cores["limpo"]}')
+sleep(1)
+print(linha_amarela)
+
+if pergunta == num:
+    print(f'{cores["verde"]}Acertou!!!{cores["limpo"]}')
+else:
+    print(f'{cores["vermelho"]}Errou!!!')
+    print(f'Tente novamente...')
+
+print(linha_verde)
+print(f'O número era {cores["verde"]}{num}{cores["limpo"]}')
+print(linha_verde)
 print('\033[31m__FIM__\033[m')
