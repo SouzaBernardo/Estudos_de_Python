1c956ef82f602f2f1ea65f10a76f583093edc3c7
Bernardo P S Souza <beportoss@gmail.com>
2021-01-16 21:59:10 -0300

    colorido e diminui os comentários

--- exercícios_basicos/ex022_nome.py
+++ exercícios_basicos/ex022_nome.py
@@ -1,13 +1,10 @@
 # Dizer o primeiro e ultimo nome
-
-# Pdeir nome
 nome = str(input('Nome:')).strip().split()
-
-# Identificar o primeiro e ultimo
 primeiro_nome = nome[0].capitalize()
 ultimo_nome = nome[-1].capitalize()
 
-# Resultado
-print(f'O primeiro nome é {primeiro_nome}')
-print(f'E, o ultimo nome é {ultimo_nome}')
+print('\033[32m-=' * 10 + '\033[m')
+print(f'O primeiro nome é \033[33m{primeiro_nome}\033[m.')
+print(f'E, o ultimo nome é \033[33m{ultimo_nome}\033[m.')
+print('\033[32m-=' * 10 + '\033[m')
 print('__FIM__')
