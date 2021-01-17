4dbf2a9606673fef1c7bd2a6a6ce2f4043203a1c
Bernardo P S Souza <beportoss@gmail.com>
2021-01-17 15:48:57 -0300

    colorido, menos comentario e preco formatado

--- exercícios_basicos/ex026_200km.py
+++ exercícios_basicos/ex026_200km.py
@@ -1,24 +1,32 @@
 # Preço de uma viagem
 from time import sleep
-from colorama import init
-init()
-print('\033[33m-=-' * 11)
-print('\033[35mPreço da viagem...')
-print('\033[33m-=-\033[m' * 11)
-# Pergunta a distância
+
+cores = {
+    'limpo':'\033[m',
+    'verde':'\033[32m',
+    'amarelo':'\033[33m'
+}
+linha_verde = f'{cores["verde"]}-=-' * 11 + f'{cores["limpo"]}'
+
+print(linha_verde)
+print(f'{cores["amarelo"]}Preço da viagem...{cores["limpo"]}')
+print(linha_verde)
+
 km = str(input('Digite a distância de sua viagem: ')).strip()
-km = float(km.split()[0]) # Pega somente o número
-print('\033[33m-=-' * 11)
-sleep(0.5)
-# Time...
-print('\033[35mANALISANDO...')
-print('\033[33m-=-\033[m' * 11)
+km = float(km.split()[0])
+print(linha_verde)
+
 sleep(1)
-# Calcular preço
-if km <= 200: # Para menor ou igual a 200
-    preco = km * 0.5 # Cada Km multiplicado por R$0,50
+
+print(f'{cores["amarelo"]}ANALISANDO...')
+print(linha_verde)
+
+sleep(1)
+
+if km <= 200:
+    preco = km * 0.5
 else:
-    preco = km * 0.45 # Cada km multiplicado por R$0.45
-# Indica o custo
-print(f'Sua viagem vai custar: \033[35mR${preco}')
-print('\033[33m-=-\033[m' * 11)
+    preco = km * 0.45
+
+print(f'Sua viagem vai custar: {cores["amarelo"]}R${preco:.2f}{cores["limpo"]}')
+print(linha_verde)
