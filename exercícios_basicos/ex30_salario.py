e2f88f4e77ea722ac8c401e179ddb990ac771c9f
Bernardo P S Souza <beportoss@gmail.com>
2021-01-19 14:09:28 -0300

    colorido

--- exercícios_basicos/ex30_salario.py
+++ exercícios_basicos/ex30_salario.py
@@ -1,27 +1,27 @@
 # Aumento de salário
 cores = {
     'limpo':'\033[m',
-    '':'\033[m',
+    'verde':'\033[32m',
     'amarelo':'\033[33m',
 }
+
+linha = f'{cores["amarelo"]}-=' * 18 + f'{cores["limpo"]}'
+
+print(linha)
 salario = float(input('Qual o seu salário? R$'))
-print('------------------------------------')
-print('Seu salário é R${:.2f}'.format(salario))
+print(f'{cores["verde"]}Seu salário é R${salario:.2f}{cores["limpo"]}')
+print(linha)
 
 if (salario >= 1250):
+    porcentagem = '10%'
     porcentagem_aumento = float(salario * 10) / 100
     novo_salario = float(salario + porcentagem_aumento)
-    print('------------------------------------')
-    print('Você teve um aumento de 10%, ele é R${:.2f}'.format(porcentagem_aumento))
-    print('------------------------------------')
-    print('Seu salário atual ficou R${:.2f}'.format(novo_salario))
-    print('------------------------------------')
-else: 
+else:
+    porcentagem = '15%'
     porcentagem_aumento = float(salario * 15) / 100
     novo_salario = float(salario + porcentagem_aumento)
-    print('------------------------------------')
-    print('Você teve o aumento de R${:.2f}'.format(porcentagem_aumento))
-    print('------------------------------------')
-    print('Seu salário atual é: R${:.2f}'.format(novo_salario))
-    print('------------------------------------')
-print('^__FIM__^')
+
+print(f'Você teve um {cores["verde"]}aumento{cores["limpo"]} de {cores["verde"]}{porcentagem}{cores["limpo"]}, ele é {cores["verde"]}R${porcentagem_aumento:.2f}{cores["limpo"]}.')
+print(linha)
+print(f'Seu {cores["verde"]}novo salário{cores["limpo"]} ficou {cores["verde"]}R${novo_salario:.2f}{cores["limpo"]}.')
+print(linha)
