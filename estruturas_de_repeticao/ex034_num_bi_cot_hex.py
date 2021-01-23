c78e5dbfc5a0c9e63bf9ca5327d7f6ce91def99b
Bernardo P S Souza <beportoss@gmail.com>
2021-01-23 10:42:21 -0300

    concluido, colorido e indica  erro com escolhas >3

--- estruturas_de_repeticao/ex034_num_bi_cot_hex.py
+++ estruturas_de_repeticao/ex034_num_bi_cot_hex.py
@@ -4,6 +4,7 @@ from time import sleep
 
 cores = {
     'limpo':'\033[m',
+    'vermelho':'\033[31m',
     'verde':'\033[32m',
     'amarelo':'\033[33m',
 }
@@ -11,8 +12,8 @@ cores = {
 linha = f'{cores["amarelo"]}-=' * 30 + f'{cores["limpo"]}'
 
 print(linha)
-print(f'{cores["verde"]}Olá! Escolha um {cores["amarelo"]}número inteiro{cores["verde"]} e vamos converter suas bases.{cores["limpo"]}')
-numero_escolhido = int(input(f'{cores["amarelo"]}Digite o número:'))
+print(f'{cores["verde"]}Olá! Escolha um {cores["amarelo"]}número inteiro{cores["verde"]} e vamos converter suas bases.{cores["amarelo"]}')
+numero_escolhido = int(input('Digite o número:'))
 print(linha)
 print('Escolha a base que você quer converter...')
 print('[1] - Binário')
@@ -25,17 +26,19 @@ print(linha)
 print(f'{cores["verde"]}ANALISANDO...{cores["limpo"]}')
 print(linha)
 sleep(1)
-
-if escolha_da_base == 1:
-    numero_escolhido_convertido = bin(numero_escolhido)
-    print(f'{cores["verde"]}Convertemos seu número para {cores["amarelo"]}BINÁRIO{cores["limpo"]}!')
-    print(f'Seu número ({cores["verde"]}{numero_escolhido}{cores["limpo"]}) em binário é {cores["verde"]}{numero_escolhido_convertido}{cores["limpo"]}.')
-elif escolha_da_base == 2:
-    numero_escolhido_convertido = oct(numero_escolhido)
-    print(f'{cores["verde"]}Convertemos seu número para {cores["amarelo"]}OCTAL{cores["limpo"]}!')
-    print(f'Seu número ({cores["verde"]}{numero_escolhido}{cores["limpo"]}) em octal é {cores["verde"]}{numero_escolhido_convertido}{cores["limpo"]}.')
+if escolha_da_base > 3:
+    print(f'{cores["vermelho"]}Este número não é válido! TENTE DE NOVO!{cores["limpo"]}')
 else:
-    numero_escolhido_convertido = hex(numero_escolhido)
-    print(f'{cores["verde"]}Convertemos seu número para {cores["amarelo"]}Hexadecimal{cores["limpo"]}!')
-    print(f'Seu número ({cores["verde"]}{numero_escolhido}{cores["limpo"]}) em Hexadecimal é {cores["verde"]}{numero_escolhido_convertido}{cores["limpo"]}.')
+    if escolha_da_base == 1:
+        numero_escolhido_convertido = bin(numero_escolhido)
+        print(f'{cores["verde"]}Convertemos seu número para {cores["amarelo"]}BINÁRIO{cores["limpo"]}!')
+        print(f'Seu número ({cores["verde"]}{numero_escolhido}{cores["limpo"]}) em binário é {cores["verde"]}{numero_escolhido_convertido}{cores["limpo"]}.')
+    elif escolha_da_base == 2:
+        numero_escolhido_convertido = oct(numero_escolhido)
+        print(f'{cores["verde"]}Convertemos seu número para {cores["amarelo"]}OCTAL{cores["limpo"]}!')
+        print(f'Seu número ({cores["verde"]}{numero_escolhido}{cores["limpo"]}) em octal é {cores["verde"]}{numero_escolhido_convertido}{cores["limpo"]}.')
+    else:
+        numero_escolhido_convertido = hex(numero_escolhido)
+        print(f'{cores["verde"]}Convertemos seu número para {cores["amarelo"]}Hexadecimal{cores["limpo"]}!')
+        print(f'Seu número ({cores["verde"]}{numero_escolhido}{cores["limpo"]}) em Hexadecimal é {cores["verde"]}{numero_escolhido_convertido}{cores["limpo"]}.')
 print(linha)
