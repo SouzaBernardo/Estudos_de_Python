e00cd7379622916d531b982da7f0e20eb380c7c3
Bernardo P S Souza <beportoss@gmail.com>
2021-01-22 21:30:40 -0300

    meio colorido

--- estruturas_de_repeticao/ex034_num_bi_cot_hex.py
+++ estruturas_de_repeticao/ex034_num_bi_cot_hex.py
@@ -1,6 +1,13 @@
 # Escolher um número e se que ele em Binário, Octal ou Hexadecimal
 
-print('\033[32mOlá! Escolha um número inteiro e vamos converter suas bases.\033[m')
+from time import sleep
+
+cores = {
+    'limpo':'\033[m',
+    'verde':'\033[32m',
+    'limpo':'\033[m',
+}
+print(f'{cores["verde"]}Olá! Escolha um número inteiro e vamos converter suas bases.{cores["limpo"]}')
 numero_escolhido = int(input('Digite o número:'))
 
 print('Escolha a base que você quer converter...')
@@ -12,12 +19,13 @@ escolha_da_base = int(input('Qual base que deseja?'))
 
 if escolha_da_base == 1:
     numero_escolhido_convertido = bin(numero_escolhido)
-    print('\033[32mANALISANDO...\033[m')
-    print('Convertemos seu número para \033[32mBINÁRIO\033[m!')
+    print(f'{cores["verde"]}ANALISANDO...{cores["limpo"]}')
+    print(f'Convertemos seu número para {cores["verde"]}BINÁRIO{cores["limpo"]}!')
     print(f'Seu número ({numero_escolhido}) em binário é \033[32m{numero_escolhido_convertido}\033[m.')
 elif escolha_da_base == 2:
     numero_escolhido_convertido = oct(numero_escolhido)
-    print('\033[32mANALISANDO...\033[m')
+    # add time.sleep
+    print(f'{cores["verde"]}...{cores["limpo"]}')
     print('Convertemos seu número para \033[32mOCTAL\033[m!')
     print(f'Seu número ({numero_escolhido}) em octal é \033[32m{numero_escolhido_convertido}\033[m.')
 else:
