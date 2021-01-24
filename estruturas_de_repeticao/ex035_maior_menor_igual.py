0996cc4a2c885dc45ffdf1a204d41bc6336738c3
Bernardo P S Souza <beportoss@gmail.com>
2021-01-24 09:55:19 -0300

    cores e linha

--- estruturas_de_repeticao/ex035_maior_menor_igual.py
+++ estruturas_de_repeticao/ex035_maior_menor_igual.py
@@ -1,11 +1,21 @@
 # Dizer qual número é maior ou menor
+cores = {
+    'limpo':'\033[m',
+    'vermelho':'\033[31m',
+    'verde':'\033[32m',
+    'amarelo':'\033[33m',
+    'azul':'\033[34m',
+}
+linha = f'{cores["amarelo"]}-=' * 10 + f'{cores["limpo"]}'
+
+print(linha)
 print('\033[31mVamos analisar dois números?!\033[m')
 print('Primeiro eu preciso que você me diga quais são eles!')
 
 pri_num = input('Primeiro número:')
 seg_num = input('Segundo número:')
 
-color = '\033[33m=-=\033[m'
+
 menor = pri_num
 maior = pri_num
 
@@ -17,7 +27,8 @@ if seg_num != pri_num:
     else:
         maior = seg_num
         print('O \033[31msegundo\033[m número é \033[31mmaior\033[m')
-    print(f'{color} O número \033[34mmaior\033[m é \033[34m{maior}\033[m. {color}')
-    print(f'{color} O  número \033[32mmenor\033[m é \033[32m{menor}\033[m. {color}')
+
+    print(f'O número \033[34mmaior\033[m é \033[34m{maior}\033[m.')
+    print(f'O  número \033[32mmenor\033[m é \033[32m{menor}\033[m.')
 else:
     print('Os números \033[31msão iguais\033[m!')
