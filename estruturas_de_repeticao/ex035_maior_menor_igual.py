375b53d98f8a52ee8a60996ff1bf9b8a0642f435
Bernardo P S Souza <beportoss@gmail.com>
2021-01-24 10:18:42 -0300

    colorido e menor

--- estruturas_de_repeticao/ex035_maior_menor_igual.py
+++ estruturas_de_repeticao/ex035_maior_menor_igual.py
@@ -1,34 +1,41 @@
 # Dizer qual número é maior ou menor
 cores = {
-    'limpo':'\033[m',
-    'vermelho':'\033[31m',
-    'verde':'\033[32m',
-    'amarelo':'\033[33m',
-    'azul':'\033[34m',
+    'limpo': '\033[m',
+    'vermelho': '\033[31m',
+    'verde': '\033[32m',
+    'amarelo': '\033[33m',
+    'azul': '\033[34m',
 }
-linha = f'{cores["amarelo"]}-=' * 10 + f'{cores["limpo"]}'
+
+linha = f'{cores["amarelo"]}-=' * 27 + f'{cores["limpo"]}'
+opcoes = [f'{cores["vermelho"]}primeiro{cores["limpo"]}', f'{cores["verde"]}segundo{cores["limpo"]}',]
 
 print(linha)
-print('\033[31mVamos analisar dois números?!\033[m')
-print('Primeiro eu preciso que você me diga quais são eles!')
+print('Vamos analisar dois números?!')
+print(f'Primeiro eu preciso que você me diga quais são eles!')
 
+print(linha + f'{cores["vermelho"]}')
 pri_num = input('Primeiro número:')
+
+print(linha + f'{cores["verde"]}')
 seg_num = input('Segundo número:')
 
+print(linha)
 
+selecionado_menor = opcoes[0]
+selecionado_maior = opcoes[0]
 menor = pri_num
 maior = pri_num
 
 if seg_num != pri_num:
-
     if seg_num < pri_num:
         menor = seg_num
-        print('O \033[31mprimeiro\033[m número é \033[31mmaior\033[m')
+        selecionado_menor = opcoes[1]
     else:
         maior = seg_num
-        print('O \033[31msegundo\033[m número é \033[31mmaior\033[m')
-
-    print(f'O número \033[34mmaior\033[m é \033[34m{maior}\033[m.')
-    print(f'O  número \033[32mmenor\033[m é \033[32m{menor}\033[m.')
+        selecionado_maior = opcoes[1]
 else:
-    print('Os números \033[31msão iguais\033[m!')
+    print(f'Os números {cores["vermelho"]}SÃO IGUAIS{cores["limpo"]}!')
+
+print(f'{selecionado_maior}({maior}) é {cores["azul"]}maior{cores["limpo"]}')
+print(f'{selecionado_menor}({menor}) é {cores["azul"]}menor{cores["limpo"]}')
