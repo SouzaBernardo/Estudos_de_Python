472d82cb48e03bd7ebbedbb3ab889727c0350d89
Bernardo P S Souza <beportoss@gmail.com>
2021-01-28 15:26:56 -0300

    menos comentarios

--- estruturas_de_repeticao/ex039_calcular_IMC.py
+++ estruturas_de_repeticao/ex039_calcular_IMC.py
@@ -1,13 +1,14 @@
 # Calcular o IMC de uma pessoa
 print('Vamos calcular seu IMC!')
+
 peso_kg = str(input('Preciso saber seu peso, em Quilogramas: ')).strip()
 alt_metros = str(input('Agora, preciso saber sua altura, em metros: ')).strip()
-# Transformar para Float
+
 peso_kg = float(peso_kg.split()[0])
 alt_metros = float(alt_metros.split()[0])
-# Calcula IMC
+
 imc_pessoa = peso_kg / (alt_metros ** 2)
-# Verificar o IMC
+
 if imc_pessoa < 18.5:
     print('Peso Baixo')
 elif imc_pessoa >= 18.5 or imc_pessoa <= 24.9:
@@ -20,4 +21,3 @@ elif imc_pessoa >= 35 or imc_pessoa <= 39.9:
     print('Obesidade Severa (Grau II)')
 else:
     print('Obesidade Mórbida (Grau III)')
-print('__FIM__')
