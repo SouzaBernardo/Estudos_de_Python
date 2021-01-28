90d0b4e1694085ae037262da8537d65da2110465
Bernardo P S Souza <beportoss@gmail.com>
2021-01-28 15:26:27 -0300

    menos comentários

--- estruturas_de_repeticao/ex038_tipo_triangulos.py
+++ estruturas_de_repeticao/ex038_tipo_triangulos.py
@@ -1,31 +1,28 @@
 # Verificar se as retas formam um triangulo, se sim, qual?
 print('Vamos analisar 3 retas? preciso saber a medida delas!')
 
-# Pegar as medidas
 pri_reta = input('Medida da primeira reta:').strip().split()
 seg_reta = input('Medida da segunda reta:').strip().split()
 ter_reta = input('Medida da terceira reta:').strip().split()
 
-# Transformar as medidas para FLOAT
 pri_reta = float(pri_reta[0])
 seg_reta = float(seg_reta[0])
 ter_reta = float(ter_reta[0])
 
-# Saber qual é o MENOR:
 if seg_reta < pri_reta and seg_reta < ter_reta:
     menor = seg_reta
 elif ter_reta < pri_reta and ter_reta < seg_reta:
     menor = ter_reta
 else:
     menor = pri_reta
-# Saber quem é o MAIOR:
+
 if seg_reta > pri_reta and seg_reta > ter_reta:
-    maior = seg_reta # Maior é a reta 2
+    maior = seg_reta 
 elif ter_reta > pri_reta and ter_reta > seg_reta:
-    maior = ter_reta # Maior é a reta 3
+    maior = ter_reta 
 else:
     maior = pri_reta
-# Saber quem é o MEDIANO:
+
 if seg_reta > menor and seg_reta < maior:
     media = seg_reta
 elif ter_reta > menor and ter_reta < maior:
@@ -33,19 +30,17 @@ elif ter_reta > menor and ter_reta < maior:
 else:
     media = pri_reta
 
-# Verificar se FAZEM UM TRIANGULO:
 if menor + media > maior:
-    # Definir equilatero(=TUDO IGUAL=) e escaleno(!TUDO DIFERENTE=)
+    # Equilatero(=TUDO IGUAL=), Escaleno(!TUDO DIFERENTE=)
+
     equilatero = pri_reta == seg_reta and pri_reta == ter_reta
     escaleno = pri_reta != seg_reta and pri_reta != ter_reta and seg_reta != ter_reta
-    # Verificar o TIPO de TRIANGULO:
-    if equilatero == True: # Para um equilatero
+    
+    if equilatero == True:
         print('Este triangulo é um equilatero')
-    elif escaleno == True: # Para um escaleno
+    elif escaleno == True: 
         print('Este é um triangulo escaleno')
-    else: # Para o isoceles
+    else:
         print('Este é um triangulo isoceles')
-else: # Caso não possa formar um triangulo
+else: 
     print('NÃO forma um triangulo')
-# Fim
-print('__FIM__')
