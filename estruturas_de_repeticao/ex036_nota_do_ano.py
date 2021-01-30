4d7f462eb64170b1d85ce4b263600ea4ec694d7e
Bernardo P S Souza <beportoss@gmail.com>
2021-01-29 23:44:49 -0300

    colorido

--- estruturas_de_repeticao/ex036_nota_do_ano.py
+++ estruturas_de_repeticao/ex036_nota_do_ano.py
@@ -1,11 +1,11 @@
 # Dizer a média anual de um aluno e dizer se ele passou.
 cores = {
     'limpa':'\033[m',
-    'azul':'\033[34m',
+    'verde':'\033[32m',
     'amarelo':'\033[33m',
-    'verde':'\033[32m'
+    'azul':'\033[34m',
 }# arrumar as cores
-print('\033[32mBOLETIM:\033[m')
+print(f'{cores["verde"]}BOLETIM:{cores["limpo"]}')
 print('Para saber se você passou, preciso das suas notas do 1° e 2° semestre...')
 
 pri_sem = float(input('Primeiro semestre:'))
@@ -13,13 +13,13 @@ seg_sem = float(input('Segundo semestre:'))
 med_ano = (pri_sem + seg_sem) / 2
 
 if med_ano <= 5:
-    print('Você \033[31mREPROVOU\033[m!')
-    print(f'Tu ficou com a nota de \033[31m{med_ano:.2f}\033[m.')
+    print(f'Você {cores["vermelho"]}REPROVOU{cores["limpo"]}!')
+    print(f'Tu ficou com a nota de {cores["vermelho"]}{med_ano:.2f}{cores["limpo"]}.')
 elif med_ano < 7:
-    print('Você ficou de \033[31mRECUPERAÇÃO\033[m!')
-    print(f'Devido a sua nota que foi de \033[31m{med_ano}\033[m.')
+    print(f'Você ficou de {cores["vermelho"]}RECUPERAÇÃO{cores["limpo"]}!')
+    print(f'Devido a sua nota que foi de {cores["vermelho"]}{med_ano}{cores["limpo"]}.')
 elif med_ano < 0:
-    print('\033[31mAlgo está errado, tente de novo!\033[m')
+    print(f'{cores["vermelho"]}Algo está errado, tente de novo!{cores["limpo"]}')
 else:
-    print('Você ficou com a \033[32mmédia maior que 7\033[m.')
-    print(f'TU PASSOU! Com a nota de \033[32m{med_ano:.2f}\033[m.')
+    print(f'Você ficou com a {cores["verde"]}média maior que 7{cores["limpo"]}.')
+    print(f'TU PASSOU! Com a nota de {cores["verde"]}{med_ano:.2f}{cores["limpo"]}.')
