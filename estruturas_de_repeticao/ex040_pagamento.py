afe4f1207b378713b1486e1213483bd21f1f2a28
Bernardo P S Souza <beportoss@gmail.com>
2021-02-17 22:21:08 -0300

    falta colorir e arrumar a 3 opcao

--- estruturas_de_repeticao/ex040_pagamento.py
+++ estruturas_de_repeticao/ex040_pagamento.py
@@ -1,49 +1,62 @@
 # Forma de pagamente e condições para pagamento
-from time import sleep
+# from time import sleep
 cores = {
     'limpo':'\033[m',
     'verde':'\033[32m',
     'amarelo':'\033[33m',
     'azul':'\033[34m',
 }# arrumar as cores
+
+linha = f'{cores["amarelo"]}-=' * 30 + f'{cores["limpo"]}'
+opcoes_de_pagamento = ['à vista dinheiro/cheque: 10% de desconto', 'à vista Cartão: 5% de desconto.', '3x ou mais no cartão: 20% de juros']
+
+print(linha)
+
 print('PAGAMENTO!')
 valor_produto = input('Valor do produto: R$').strip()
 
-sleep(1)
+print(linha)
 
 print('Formas de pagamento:')
-print('[1] - à vista dinheiro/cheque: 10% de desconto')
-print('[2] - à vista Cartão: 5% de desconto')
-print('[3] - 3x ou mais no cartão: 20% de juros')
-sleep(1)
-
+print(f'[1] - {opcoes_de_pagamento[0]}')
+print(f'[2] - {opcoes_de_pagamento[1]}')
+print(f'[3] - {opcoes_de_pagamento[2]}')
 forma_pagamento = int(input('Qual a forma de pagamento (somente o número)?'))
 
 if forma_pagamento < 1 or forma_pagamento > 3:
     print('Você não escolheu uma opção...')
 else:
     if forma_pagamento == 1:
-        print('Escolha 1!')
-        print('À vista no dinheiro ou cheque')
+        escolha = '1'
+        opcoes_de_pagamento = opcoes_de_pagamento[0]
+
         valor_produto = float(valor_produto) - (float(valor_produto) / 10)
-        print(f'Nessa opção o produto sai por R${valor_produto}.')
+        print(f'Nessa opção o produto sai por R${valor_produto}.') # valor
         
     elif forma_pagamento == 2:
-        print('Escolha 2!')
-        print('À vista no cartão.')
-        valor_produto = float(valor_produto) - (float(valor_produto) * 5 / 100)
+        escolha = '2'
+        opcoes_de_pagamento = opcoes_de_pagamento[1]
+
+        valor_produto = float(valor_produto) - (float(valor_produto) * 5 / 100) # valor
         print(f'Nessa opção o produto sai por R${valor_produto}.')
 
     else:
-        print('Escolha 3!')
-        print('No cartão parcelado.')
-        juros_produto_ = float(valor_produto) + (float(valor_produto) * 2 / 10)
-        meses_parcelados = input('Meses parcelados:').strip()
+        escolha = '3'
+        opcoes_de_pagamento = opcoes_de_pagamento[2]
 
-        valor_juros_mensal = juros_produto_ / float(meses_parcelados.split()[0])
-        valor_produto_parcelado = float(valor_produto) / float(meses_parcelados.split()[0])
-        produto_mensal_jurado = valor_produto_parcelado + valor_juros_mensal
+        valor_produto = float(valor_produto) + (float(valor_produto) * 2 / 10)
+
+        meses_parcelados = input('Meses parcelados:').strip()
+        if meses_parcelados < 3:
+            print('Erro! Deve ser acima de 3 meses!')
+        else: # reparar daqui para baixo...
+            valor_juros_mensal = valor_produto / float(meses_parcelados.split()[0])
+            valor_produto_parcelado = float(valor_produto) / float(meses_parcelados.split()[0])
+            produto_mensal_jurado = valor_produto_parcelado + valor_juros_mensal
+
         print(f'Nessa opção o produto sai por R${valor_produto_parcelado:.2f} ao mês.')
         print(f'Este produto foi parcelado em {meses_parcelados} meses.')
 
-
+print(f'Escolha {escolha}!')
+print(f'Forma de pagamento: {opcoes_de_pagamento}')
+print(f'Nesta forma o produto sai por: R${valor_produto}')
