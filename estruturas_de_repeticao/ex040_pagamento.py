d43b5767c106f9e3a8d71dd412757a36eac497e9
Bernardo P S Souza <beportoss@gmail.com>
2021-02-17 21:44:25 -0300

    consegui fazer com que a validação venha antes

--- estruturas_de_repeticao/ex040_pagamento.py
+++ estruturas_de_repeticao/ex040_pagamento.py
@@ -19,29 +19,31 @@ sleep(1)
 
 forma_pagamento = int(input('Qual a forma de pagamento (somente o número)?'))
 
-if forma_pagamento == 1:
-    print('Escolha 1!')
-    print('À vista no dinheiro ou cheque')
-    valor_produto = float(valor_produto) - (float(valor_produto) / 10)
-    print(f'Nessa opção o produto sai por R${valor_produto}.')
-    
-elif forma_pagamento == 2:
-    print('Escolha 2!')
-    print('À vista no cartão.')
-    valor_produto = float(valor_produto) - (float(valor_produto) * 5 / 100)
-    print(f'Nessa opção o produto sai por R${valor_produto}.')
-
-elif forma_pagamento == 3:
-    print('Escolha 3!')
-    print('No cartão parcelado.')
-    juros_produto_ = float(valor_produto) + (float(valor_produto) * 2 / 10)
-    meses_parcelados = input('Meses parcelados:').strip()
-
-    valor_juros_mensal = juros_produto_ / float(meses_parcelados.split()[0])
-    valor_produto_parcelado = float(valor_produto) / float(meses_parcelados.split()[0])
-    produto_mensal_jurado = valor_produto_parcelado + valor_juros_mensal
-    print(f'Nessa opção o produto sai por R${valor_produto_parcelado:.2f} ao mês.')
-    print(f'Este produto foi parcelado em {meses_parcelados} meses.')
-
+if forma_pagamento < 1 or forma_pagamento > 3:
+    print('Você não escolheu uma opção...')
 else:
-    print('Você não escolheu uma opção...')  # fazer com que isso venha antes
+    if forma_pagamento == 1:
+        print('Escolha 1!')
+        print('À vista no dinheiro ou cheque')
+        valor_produto = float(valor_produto) - (float(valor_produto) / 10)
+        print(f'Nessa opção o produto sai por R${valor_produto}.')
+        
+    elif forma_pagamento == 2:
+        print('Escolha 2!')
+        print('À vista no cartão.')
+        valor_produto = float(valor_produto) - (float(valor_produto) * 5 / 100)
+        print(f'Nessa opção o produto sai por R${valor_produto}.')
+
+    else:
+        print('Escolha 3!')
+        print('No cartão parcelado.')
+        juros_produto_ = float(valor_produto) + (float(valor_produto) * 2 / 10)
+        meses_parcelados = input('Meses parcelados:').strip()
+
+        valor_juros_mensal = juros_produto_ / float(meses_parcelados.split()[0])
+        valor_produto_parcelado = float(valor_produto) / float(meses_parcelados.split()[0])
+        produto_mensal_jurado = valor_produto_parcelado + valor_juros_mensal
+        print(f'Nessa opção o produto sai por R${valor_produto_parcelado:.2f} ao mês.')
+        print(f'Este produto foi parcelado em {meses_parcelados} meses.')
+
+
