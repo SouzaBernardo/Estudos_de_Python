a83bcd7a5e7f847c1cd720c8c67191bd46236f8a
Bernardo P S Souza <beportoss@gmail.com>
2021-02-18 21:16:54 -0300

    cores que ja usei

--- estruturas_de_repeticao/ex040_pagamento.py
+++ estruturas_de_repeticao/ex040_pagamento.py
@@ -2,9 +2,9 @@
 # from time import sleep
 cores = {
     'limpo':'\033[m',
-    'vermelho':'\033[31m',
-    'verde':'\033[32m',
-    'amarelo':'\033[33m',
+    'vermelho':'\033[31m', # 
+    'verde':'\033[32m', # 
+    'amarelo':'\033[33m', #
     'azul':'\033[34m',
 }# arrumar as cores
 
