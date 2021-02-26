7a93517193af22e5847348a98653390cce65739c
Bernardo P S Souza <beportoss@gmail.com>
2021-02-26 18:18:26 -0300

    ex02

--- estrutura_for/ex02-num-par.py
+++ estrutura_for/ex02-num-par.py
@@ -3,11 +3,11 @@ from math import ceil, floor
 x = int(input('Digite um número:'))
 y = int(x / 2)
 if y % 2 != 0:
-    y = floor(y)  # analizar aqui
+    y = floor(y + 1)
 for intervalo in range(1, y):
     if x % 2 == 0:
         x -= 2
     else:
-        x -= 1  # números impares não entregam o dois
+        x -= 1
     print(x)
 print(y)
