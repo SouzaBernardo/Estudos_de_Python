50f38ca2baa057d2fbdbfe2adfdb00c0b0b9bf4c
Bernardo Porto e Silva Souza <72446326+BEp0@users.noreply.github.com>
2021-03-17 13:00:11 -0300

    revisei a PA

--- estrutura_for/ex06-pa.py
+++ estrutura_for/ex06-pa.py
@@ -1,23 +1,23 @@
 # Progreção aritmética
 '''
-
 - primeiro termo e a razao de uma pa 10 primeiro termos
-
-
--  inicial
-
-- diminuir o segundo com o primeiro
+    -  inicial
+    - diminuir o segundo com o primeiro
 '''
 
 def main():
-    # estavs errado 
+    linha = '-=' * 16
+    print(linha)
     term = int(input('Digite o primeiro termo da PA:'))
     r = int(input('Digite a razão da PA:'))
+    print(linha)
+    print('Os 10 primeiros temos da PA são:')
 
     for i in range(0, 10):
-        print(term)
+        print(term, end=' ')
         term += r
-        
+
+    print(f'\n{linha}')        
 
 if __name__ == "__main__":
     main()
