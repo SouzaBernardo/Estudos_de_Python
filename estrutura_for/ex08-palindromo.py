84eff00afdae438c8c76ae8f00b3b4659a1b0f5f
Bernardo Porto e Silva Souza <72446326+BEp0@users.noreply.github.com>
2021-03-17 12:51:32 -0300

    linhha

--- estrutura_for/ex08-palindromo.py
+++ estrutura_for/ex08-palindromo.py
@@ -4,6 +4,8 @@
 '''
 # Revisado ...
 def main():
+    linha = '-=' * 16
+    print(linha)
     frase = input('Digite a palavra escolhida:').strip().upper()
     palavras = frase.split()
     junto = ''.join(palavras)
@@ -16,13 +18,13 @@ def main():
         # -1 => O segundo -1 é para ele começar do final, posição -1
         # -1 => O último -1 é para ele ir voltando uma letra 
     '''
-
-    print(f'O inverso de {junto} é: {inverso}.')
-
+    print(linha)
+    print(f'O inverso de {junto} é: \033[32m{inverso}\033[m.')
+    print(linha)
     if inverso == junto:
         print('É um palindromo!')
     else:
         print('Não é um palindromo...')
-        
+    print(linha)
 if __name__ == "__main__":
     main()
