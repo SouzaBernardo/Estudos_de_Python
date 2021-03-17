c549995c1fa71312b8f4f257a6a89e8534d33f72
Bernardo Porto e Silva Souza <72446326+BEp0@users.noreply.github.com>
2021-03-17 12:28:05 -0300

    exercício antes da revisão(ex0088)

--- estrutura_for/ex08-palindromo.py
+++ estrutura_for/ex08-palindromo.py
@@ -2,17 +2,16 @@
     DIZER SE É UM  palindromo -> de tras pra frente
         - desconsiderar os espaços e os acentos
 '''
-
+# antes da revisão ...
 def main():
-    palavra = input('Digite a palavra escolhida:')
+    palavra = input('Digite a palavra escolhida:').upper()
     lista = list(palavra)
     #como inverter uma lista ?
-    #inverso_lista = reversed(lista)
-    if lista == 0:
+    inverso_lista = lista[::-1] # assim podemos inverter uma lista...
+    if lista == inverso_lista:
         print('É um palindromo!')
     else:
         print('Não é um palindromo!')
-    print(lista, palavra)
 
 if __name__ == "__main__":
     main()
