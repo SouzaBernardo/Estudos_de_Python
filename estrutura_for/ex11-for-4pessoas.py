b0402465adf07561f89500f729da49faf66c4adc
Bernardo P S Souza <beportoss@gmail.com>
2021-03-28 20:03:06 -0300

    fim
    ja esta dizendo a media das idades, quantas mulheres são menores de 21 anos e qual pessoa é a mais velha, dizendo seu nome e sua idade

--- estrutura_for/ex11-for-4pessoas.py
+++ estrutura_for/ex11-for-4pessoas.py
@@ -3,12 +3,15 @@
     ## mostrar
         -media da idade 
         -mais velho
-        -quantas mulheres menores de 20anos
+        -quantas mulheres menores de 21anos
 '''
 #média ok
 
 soma_idades = 0
 media = 0
+mulheres_menores_21 =0
+
+
 for i in range(1, 5):
     nome = str(input('Digite o nome:'))
     idade = int(input('Digite a idade:'))
@@ -21,9 +24,14 @@ for i in range(1, 5):
         if idade > mais_velho[1]:
             mais_velho = nome, idade, sexo
 
+    # verificar a quantidade de mulheres menores de 21 anos
+    if mais_velho.count('F') and idade < 21:
+        mulheres_menores_21 += 1
+
     soma_idades += idade
 
 media = soma_idades / 4
 
 print(f'A media das idades foi: {media}')
-print(mais_velho)
+print(f'Há {mulheres_menores_21} mulheres menores de 21 anos')
+print(f'A pessoa mais velha é {mais_velho[0]} com {mais_velho[1]}')
