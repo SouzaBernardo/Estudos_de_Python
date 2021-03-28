4acf28a32aea2de642f9a7b18920bdf51e39924e
Bernardo P S Souza <beportoss@gmail.com>
2021-03-28 19:54:31 -0300

    renomeado
    renomeei e ja conclui a parte da idade, so falta dizer quantas mulheres acima de 21 anos

--- estrutura_for/ex11-for-4pessoas.py
+++ estrutura_for/ex11-for-4pessoas.py
@@ -12,10 +12,18 @@ media = 0
 for i in range(1, 5):
     nome = str(input('Digite o nome:'))
     idade = int(input('Digite a idade:'))
-    if i == 1:
-        mais_velho = nome, idade
-    #sexo = str(input('Digite o sexo (M/F):'))
-    soma_idades += idade
+    sexo = str(input('Digite o sexo (M/F):')).upper()
     
+    # identificar quem é mais velho
+    if i == 1:
+        mais_velho = nome, idade, sexo
+    else:
+        if idade > mais_velho[1]:
+            mais_velho = nome, idade, sexo
+
+    soma_idades += idade
+
 media = soma_idades / 4
+
 print(f'A media das idades foi: {media}')
+print(mais_velho)
