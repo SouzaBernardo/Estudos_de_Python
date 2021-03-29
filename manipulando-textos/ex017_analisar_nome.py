97b98143fb60b760150fa67429c07a54ebd7bbf5
Bernardo Porto e Silva Souza <72446326+BEp0@users.noreply.github.com>
2021-03-29 15:58:44 -0300

    exercício corrigido

--- manipulando-textos/ex017_analisar_nome.py
+++ manipulando-textos/ex017_analisar_nome.py
@@ -1,13 +1,13 @@
 # Pedir o nome da pessoa, e analisa-lo
 nome = str(input('\033[33mQual será o nome para analisar?\033[m')).strip()
 
-maiusculo = nome.upper()
-minusculo = nome.lower()
+#maiusculo = nome.upper()
+#minusculo = nome.lower()
 quantidade_de_letras = len(nome) - nome.count(' ')
 quantidade_letra_primeiro = len(nome.split()[0])
 
-print(f'Nome em MAIÚSCULO: \033[33m{maiusculo}\033[m.')
-print(f'Nome minúsculo: \033[33m{minusculo}\033[m.')
+print(f'Nome em MAIÚSCULO: \033[33m{nome.upper()}\033[m.')
+print(f'Nome minúsculo: \033[33m{nome.lower()}\033[m.')
 print(f'O nome tem \033[33m{quantidade_de_letras}\033[m letras.')
 print(f'O primeiro nome tem \033[33m{quantidade_letra_primeiro}\033[m letras')
 print('__FIM__')
