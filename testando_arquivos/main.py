526a65303c160c4b48e65ebbe9d2dc8f42fc888e
BEp0 <beportoss@gmail.com>
2021-04-23 10:53:42 -0300

    main

--- testando_arquivos/main.py
+++ testando_arquivos/main.py
@@ -1,16 +1,24 @@
 def criar_arquivo():
+    
+    while True:
+        iniciar = int(input('\nNúmero inicial: '))
+        final = int(input('Número final: '))
+        
+        # obriga ao inicial ser menor que o final
+        if not iniciar > final:
+            break
+        else:
+            print('     \033[31mERRO: REPITA\033[m')
 
-    iniciar = int(input('Número inicial: '))
-    final = int(input('Número final: '))
-
-    formato = str(input('Formato do arquivo (com o ponto): ')).strip().lower()
-
-    destino_dado = str(input('Destino dos exercícios (sem o nome do arquivo): ')).strip()
+    
+    formato = str(input('\nFormato do arquivo (com o ponto): ')).strip().lower()
+    destino_dado = str(input('\nDestino dos exercícios (sem o nome do arquivo): ')).strip()
 
     for i in range(iniciar, final + 1):
-        print(i)
+        print(i, end=' ')
         destino_escrever = open(f'{destino_dado}/ex{i}{formato}', 'w' )
-        
+    
+    print('\n')    
     destino_escrever.close()
         
 def main():
