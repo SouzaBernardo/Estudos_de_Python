38ed7359e9444f059795007b2faa74fba536b353
BEp0 <beportoss@gmail.com>
2021-04-23 09:46:08 -0300

    programa para criar arquivos

--- testando_arquivos/main.py
+++ testando_arquivos/main.py
@@ -1,19 +1,20 @@
-def obtemCadeiaA():
-    fonte = open('/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/7kms.pdb', 'r')
-    destino = open( '/home/souza/Documents/Programas-GitHub/Topicos-avancado/19-04/7kmsA.pdb', 'w' )
+def criar_arquivo():
+
+    iniciar = int(input('Número inicial: '))
+    final = int(input('Número final: '))
+
+    formato = str(input('Formato do arquivo (com o ponto): ')).strip().lower()
+
+    destino_dado = str(input('Destino dos exercícios (sem o nome do arquivo): ')).strip()
+
+    for i in range(iniciar, final + 1):
+        print(i)
+        destino_escrever = open(f'{destino_dado}/ex{i}{formato}', 'w' )
         
-    for i in fonte:
-        if ( i[:4] == 'ATOM' ):
-            if ( i[21] == 'A'):
-                destino.write(i)
-
-
-    destino.close()
-    fonte.close()
-
-
+    destino_escrever.close()
+        
 def main():
-    obtemCadeiaA()
+    criar_arquivo()
 
 
 if __name__ == "__main__":
