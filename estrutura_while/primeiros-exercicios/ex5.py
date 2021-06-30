da32a01d3f4462b3e6b1b6035106895afe9c7627
Bernardo Porto e Silva Souza <beportoss@gmail.com>
2021-06-30 08:36:34 -0300

    Exercicio ok, 05

--- estrutura_while/primeiros-exercicios/ex5.py
+++ estrutura_while/primeiros-exercicios/ex5.py
@@ -1,3 +1,22 @@
 '''
 ler o primeiro termo e a razão da pa, e mostrar os 10 primeiros termos
 '''
+
+
+def main():
+    print('|********PROGRESSÃO ARITMÉTICA********|')
+    termo = int(input('-> Primeiro termo da PA: '))
+    razao = int(input('-> Razão da PA: '))
+    total = 0
+    cont = 10
+    while cont > 0:
+        print(f'{termo}', end='')
+        print(' + ' if cont > 1 else ' = ', end='')
+        termo += razao
+        total += termo
+        cont -= 1
+    print(f'{total}')
+
+
+if __name__ == "__main__":
+    main()
