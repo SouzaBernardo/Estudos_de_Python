6e9d767c22bd861fd4e00a0f753d303753c22b97
Bernardo Porto e Silva Souza <72446326+BEp0@users.noreply.github.com>
2022-01-06 22:25:19 -0300

    refatoração do exercicio

--- estruturas_condicionais/ex026_200km.py
+++ estruturas_condicionais/ex026_200km.py
@@ -2,31 +2,43 @@
 from time import sleep
 
 cores = {
-    'limpo':'\033[m',
-    'verde':'\033[32m',
-    'amarelo':'\033[33m'
+    'limpo': '\033[m',
+    'verde': '\033[32m',
+    'amarelo': '\033[33m'
 }
+
 linha_verde = f'{cores["verde"]}-=-' * 11 + f'{cores["limpo"]}'
 
-print(linha_verde)
-print(f'{cores["amarelo"]}Preço da viagem...{cores["limpo"]}')
-print(linha_verde)
 
-km = str(input('Digite a distância de sua viagem: ')).strip()
-km = float(km.split()[0])
-print(linha_verde)
+def analise_preco(km):
+    if km <= 200:
+        preco = km * 0.5
+    else:
+        preco = km * 0.45
+    return preco
 
-sleep(1)
 
-print(f'{cores["amarelo"]}ANALISANDO...')
-print(linha_verde)
+def main():
 
-sleep(1)
+    print(
+        f'{linha_verde}\n{cores["amarelo"]}Preço da viagem...{cores["limpo"]}\n{linha_verde}')
 
-if km <= 200:
-    preco = km * 0.5
-else:
-    preco = km * 0.45
+    km = str(input('Digite a distância de sua viagem: ')).strip()
+    km = float(km.split()[0])
+    print(linha_verde)
 
-print(f'Sua viagem vai custar: {cores["amarelo"]}R${preco:.2f}{cores["limpo"]}')
-print(linha_verde)
+    sleep(1)
+
+    print(f'{cores["amarelo"]}ANALISANDO...')
+    print(linha_verde)
+
+    sleep(1)
+
+    preco = analise_preco(km)
+
+    print(
+        f'Sua viagem vai custar: {cores["amarelo"]}R${preco:.2f}{cores["limpo"]}\n{linha_verde}')
+
+
+if __name__ == "__main__":
+    main()
